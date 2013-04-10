f7b6e719c8fe746848bc23d5664166c67781403a
Alfredo Amatriain <geralt@gmail.com>
2013-04-10 23:30:55 +0200

    Added placeholders to form inputs. Added rdoc comments to custom inputs.

--- app/inputs/email_icon_input.rb
+++ app/inputs/email_icon_input.rb
@@ -1,5 +1,19 @@
+##
+# Output HTML for an email input form field.
+#
+# The HTML generated is compatible with Bootstrap, and it prepends an envelope icon
+# (from FontAwesome[http://fortawesome.github.io/Font-Awesome/]) before the input.
+#
+# This class is intended to be used from simple_form[https://github.com/plataformatec/simple_form] forms like this:
+#
+#   <%= simple_form_for user, wrapper: :prepend do %>
+#     <%= f.input :email, as: :email_icon %>
+#     ...
+#   <% end %>
+
 class EmailIconInput < SimpleForm::Inputs::Base
   def input
+    input_html_options[:placeholder] ||= I18n.t 'simple_form.placeholders.user.email'
     "<span class=\"add-on\"><i class=\"icon-envelope\"></i></span>#{@builder.email_field(attribute_name, input_html_options)}".html_safe
   end
 end
--- app/inputs/password_icon_input.rb
+++ app/inputs/password_icon_input.rb
@@ -1,5 +1,19 @@
+##
+# Output HTML for a password input form field.
+#
+# The HTML generated is compatible with Bootstrap, and it prepends a key icon
+# (from FontAwesome[http://fortawesome.github.io/Font-Awesome/]) before the input.
+#
+# This class is intended to be used from simple_form[https://github.com/plataformatec/simple_form] forms like this:
+#
+#   <%= simple_form_for user, wrapper: :prepend do %>
+#     <%= f.input :password, as: :password_icon %>
+#     ...
+#   <% end %>
+
 class PasswordIconInput < SimpleForm::Inputs::Base
   def input
+    input_html_options[:placeholder] ||= I18n.t 'simple_form.placeholders.user.password'
     "<span class=\"add-on\"><i class=\"icon-key\"></i></span>#{@builder.password_field(attribute_name, input_html_options)}".html_safe
   end
 end
