9367bbe0f4ca897f5de03ed2400693da7049fcf0
Alfredo Amatriain <geralt@gmail.com>
2013-04-15 20:31:31 +0200

    Fixed broken tests.

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -2,15 +2,18 @@ require 'spec_helper'
 
 describe FeedsController do
   before :each do
-    @feed = FactoryGirl.create :feed
-    login_user_for_unit
+    @user = FactoryGirl.create :user
+    login_user_for_unit @user
+    @feed1 = FactoryGirl.create :feed
+    @feed2 = FactoryGirl.create :feed
+    @user.feeds << @feed1
   end
 
   context 'GET index' do
-    it 'assigns all feeds to @feeds' do
+    it 'assigns to @feeds only feeds the user is suscribed to' do
       get :index
       response.should be_success
-      assigns(:feeds).should eq [@feed]
+      assigns(:feeds).should eq [@feed1]
     end
   end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,5 +1,9 @@
-def login_user_for_unit
+##
+# Login a user using Devise helpers
+#
+# This method is intended to be called during a unit test, normally for a controller (but not necessarily).
+
+def login_user_for_unit(user)
   @request.env['devise.mapping'] = Devise.mappings[:user]
-  user = FactoryGirl.create(:user)
   sign_in user
 end
