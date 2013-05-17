d9cbb40e04f201131b0de734c41f1d2182c56e5a
Alfredo Amatriain <geralt@gmail.com>
2013-05-17 17:40:12 +0200

    Include extensions .json.erb when rendering a template at the end of FoldersController#update and #create methods.
    It seems rails sometimes got confused and tried to render an html template if these extensions were not specified.

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -112,7 +112,7 @@ class FoldersController < ApplicationController
         old_folder.reload
         old_folder.destroy if old_folder.feeds.blank?
       end
-      render 'update', locals: {new_folder: new_folder, feed: feed, old_folder: old_folder}
+      render 'update.json.erb', locals: {new_folder: new_folder, feed: feed, old_folder: old_folder}
     end
   rescue AlreadyInFolderError
     # If feed is already associated to the folder, return 304
@@ -181,7 +181,7 @@ class FoldersController < ApplicationController
         old_folder.reload
         old_folder.destroy if old_folder.feeds.blank?
       end
-      render 'create', locals: {new_folder: new_folder, old_folder: old_folder}
+      render 'create.json.erb', locals: {new_folder: new_folder, old_folder: old_folder}
     end
 
   rescue FolderAlreadyExistsError
