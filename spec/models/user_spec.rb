cc56a56055aef4fa48562e38355c79d607087a0a
Alfredo Amatriain <geralt@gmail.com>
2013-05-20 14:11:14 +0200

    Better organization of the User model unit tests, grouping related tests in Rspec contexts.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,94 +5,104 @@ describe User do
     @user = FactoryGirl.create :user
   end
 
-  it 'returns feeds the user is suscribed to' do
-    feed1 = FactoryGirl.create :feed
-    feed2 = FactoryGirl.create :feed
-    @user.feeds << feed1 << feed2
-    @user.feeds.include?(feed1).should be_true
-    @user.feeds.include?(feed2).should be_true
+  context 'validations' do
+    it 'does not allow duplicate usernames' do
+      user_dupe = FactoryGirl.build :user, email: @user.email
+      user_dupe.valid?.should be_false
+    end
   end
 
-  it 'does not return feeds the user is not suscribed to' do
-    feed = FactoryGirl.create :feed
-    @user.feeds.include?(feed).should be_false
+  context 'relationship with feeds' do
+    it 'returns feeds the user is suscribed to' do
+      feed1 = FactoryGirl.create :feed
+      feed2 = FactoryGirl.create :feed
+      @user.feeds << feed1 << feed2
+      @user.feeds.include?(feed1).should be_true
+      @user.feeds.include?(feed2).should be_true
+    end
+
+    it 'does not return feeds the user is not suscribed to' do
+      feed = FactoryGirl.create :feed
+      @user.feeds.include?(feed).should be_false
+    end
+
+    it 'does not allow subscribing to the same feed more than once' do
+      feed = FactoryGirl.create :feed
+      @user.feeds << feed
+      @user.feeds << feed
+      @user.feeds.count.should eq 1
+      @user.feeds.first.should eq feed
+    end
   end
 
-  it 'does not allow duplicate usernames' do
-    user_dupe = FactoryGirl.build :user, email: @user.email
-    user_dupe.valid?.should be_false
+  context 'relationship with folders' do
+    it 'deletes folders when deleting a user' do
+      folder1 = FactoryGirl.build :folder
+      folder2 = FactoryGirl.build :folder
+      @user.folders << folder1 << folder2
+
+      Folder.count.should eq 2
+
+      @user.destroy
+      Folder.count.should eq 0
+    end
+
+    it 'does not allow associating to the same folder more than once' do
+      folder = FactoryGirl.build :folder, user_id: @user.id
+      @user.folders << folder
+      @user.folders << folder
+      @user.folders.count.should eq 1
+      @user.folders.first.should eq folder
+    end
   end
 
-  it 'deletes folders when deleting a user' do
-    folder1 = FactoryGirl.build :folder
-    folder2 = FactoryGirl.build :folder
-    @user.folders << folder1 << folder2
+  context 'relationship with entries' do
+    it 'retrieves all entries for all subscribed feeds' do
+      feed1 = FactoryGirl.create :feed
+      feed2 = FactoryGirl.create :feed
+      @user.feeds << feed1 << feed2
+      entry1 = FactoryGirl.build :entry, feed_id: feed1.id
+      entry2 = FactoryGirl.build :entry, feed_id: feed1.id
+      entry3 = FactoryGirl.build :entry, feed_id: feed2.id
+      feed1.entries << entry1 << entry2
+      feed2.entries << entry3
 
-    Folder.count.should eq 2
-
-    @user.destroy
-    Folder.count.should eq 0
+      @user.entries.count.should eq 3
+      @user.entries.should include entry1
+      @user.entries.should include entry2
+      @user.entries.should include entry3
+    end
   end
 
-  it 'retrieves all entries for all subscribed feeds' do
-    feed1 = FactoryGirl.create :feed
-    feed2 = FactoryGirl.create :feed
-    @user.feeds << feed1 << feed2
-    entry1 = FactoryGirl.build :entry, feed_id: feed1.id
-    entry2 = FactoryGirl.build :entry, feed_id: feed1.id
-    entry3 = FactoryGirl.build :entry, feed_id: feed2.id
-    feed1.entries << entry1 << entry2
-    feed2.entries << entry3
-
-    @user.entries.count.should eq 3
-    @user.entries.should include entry1
-    @user.entries.should include entry2
-    @user.entries.should include entry3
-  end
-
-  it 'does not allow subscribing to the same feed more than once' do
-    feed = FactoryGirl.create :feed
-    @user.feeds << feed
-    @user.feeds << feed
-    @user.feeds.count.should eq 1
-    @user.feeds.first.should eq feed
-  end
-
-  it 'does not allow associating to the same folder more than once' do
-    folder = FactoryGirl.build :folder, user_id: @user.id
-    @user.folders << folder
-    @user.folders << folder
-    @user.folders.count.should eq 1
-    @user.folders.first.should eq folder
-  end
-
-  it 'retrieves entry states for subscribed feeds' do
-    entry_state1 = FactoryGirl.build :entry_state, user_id: @user.id
-    entry_state2 = FactoryGirl.build :entry_state, user_id: @user.id
-    @user.entry_states << entry_state1
-    @user.entry_states << entry_state2
+  context 'relationship with entry states' do
+    it 'retrieves entry states for subscribed feeds' do
+      entry_state1 = FactoryGirl.build :entry_state, user_id: @user.id
+      entry_state2 = FactoryGirl.build :entry_state, user_id: @user.id
+      @user.entry_states << entry_state1
+      @user.entry_states << entry_state2
 
 
-    @user.entry_states.count.should eq 2
-    @user.entry_states.should include entry_state1
-    @user.entry_states.should include entry_state2
-  end
+      @user.entry_states.count.should eq 2
+      @user.entry_states.should include entry_state1
+      @user.entry_states.should include entry_state2
+    end
 
-  it 'deletes entry states when deleting a user' do
-    entry_state = FactoryGirl.build :entry_state, user_id: @user.id
-    @user.entry_states << entry_state
+    it 'deletes entry states when deleting a user' do
+      entry_state = FactoryGirl.build :entry_state, user_id: @user.id
+      @user.entry_states << entry_state
 
-    EntryState.count.should eq 1
-    @user.destroy
-    EntryState.count.should eq 0
-  end
+      EntryState.count.should eq 1
+      @user.destroy
+      EntryState.count.should eq 0
+    end
 
-  it 'does not allow duplicate entry states' do
-    entry_state = FactoryGirl.build :entry_state, user_id: @user.id
-    @user.entry_states << entry_state
-    @user.entry_states << entry_state
+    it 'does not allow duplicate entry states' do
+      entry_state = FactoryGirl.build :entry_state, user_id: @user.id
+      @user.entry_states << entry_state
+      @user.entry_states << entry_state
 
-    @user.entry_states.count.should eq 1
+      @user.entry_states.count.should eq 1
+    end
   end
 
 end
