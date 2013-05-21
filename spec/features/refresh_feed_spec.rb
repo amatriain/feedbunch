85a348d5fde3ad667402bcacc32552029f4dbae7
Alfredo Amatriain <geralt@gmail.com>
2013-05-21 14:41:36 +0200

    Added acceptance test to ensure that after refreshing a single feed, only unread entries are shown by default.

--- spec/features/refresh_feed_spec.rb
+++ spec/features/refresh_feed_spec.rb
@@ -8,9 +8,9 @@ describe 'refresh feeds' do
 
     @user = FactoryGirl.create :user
     @feed1 = FactoryGirl.create :feed
-    @user.feeds << @feed1
     @entry1 = FactoryGirl.build :entry, feed_id: @feed1.id
     @feed1.entries << @entry1
+    @user.feeds << @feed1
 
     login_user_for_feature @user
     visit feeds_path
@@ -42,7 +42,27 @@ describe 'refresh feeds' do
     page.should have_content entry2.title
   end
 
-  it 'only shows unread entries when refreshing a single feed'
+  it 'only shows unread entries when refreshing a single feed', js: true do
+    entry2 = FactoryGirl.build :entry, feed_id: @feed1.id
+    @feed1.entries << entry2
+    # @entry1 is read, entry2 is unread
+    entry_state1 = EntryState.where(user_id: @user.id, entry_id: @entry1.id).first
+    entry_state1.read = true
+    entry_state1.save!
+    # When refreshing the feed, fetch the new unread entry3
+    entry3 = FactoryGirl.build :entry, feed_id: @feed1.id
+    FeedClient.stub :fetch do
+      @feed1.entries << entry3
+    end
+
+    read_feed @feed1.id
+    find('a#refresh-feed').click
+
+    # entry2 and entry3 should appear, @entry1 should not appear because it's already read
+    page.should_not have_content @entry1.title
+    page.should have_content entry2.title
+    page.should have_content entry3.title
+  end
 
   it 'refreshes all subscribed feeds', js: true do
     feed2 = FactoryGirl.create :feed
