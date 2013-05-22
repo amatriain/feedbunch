5da48663fecab193fb952352ef91099d8b818e8d
Alfredo Amatriain <geralt@gmail.com>
2013-05-22 18:36:24 +0200

    Moved methods related with refreshing feeds and folders from the User model to a new module, UserRefreshHelpers.
    This continues the line of simplification of the User model, which had become bloated, by moving functions to separate modules which are included in the User model. Modules group methods with related functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,7 @@
 class User < ActiveRecord::Base
   include UserSubscriptionHelpers
   include UserFolderHelpers
+  include UserRefreshHelpers
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -95,55 +96,6 @@ class User < ActiveRecord::Base
 
     return entries
   end
-
-  ##
-  # Refresh a feed; this triggers a fetch of the feed from its server.
-  #
-  # Receives as argument the id of the feed to refresh.
-  #
-  # Returns an ActiveRecord::Relation with the unread entries for the refreshed feed; this may or may
-  # not contain new entries.
-  #
-  # If the user is not subscribed to the feed an ActiveRecord::RecordNotFound error is raised.
-
-  def refresh_feed(feed_id)
-    # ensure user is subscribed to the feed
-    feed = self.feeds.find feed_id
-
-    Rails.logger.info "User #{self.id} - #{self.email} is refreshing feed #{feed.id} - #{feed.fetch_url}"
-    FeedClient.fetch feed.id
-    entries = self.unread_feed_entries feed.id
-    return entries
-  end
-
-  ##
-  # Refresh a folder; this triggers a fetch of all the feeds in the folder.
-  #
-  # Receives as argument the id of the folder to refresh. The special value "all" means that ALL subscribed feeds
-  # will be fetched, regardless of what folder they are in (and even if they are in no folder).
-  #
-  # Returns an ActiveRecord::Relation with the unread entries for the feeds in the folder; this may or may
-  # not contain new entries.
-  #
-  # If the folder does not belong to the user, an ActiveRecord::RecordNotFound error is raised.
-
-  def refresh_folder(folder_id)
-    if folder_id == 'all'
-      Rails.logger.info "User #{self.id} - #{self.email} is refreshing all subscribed feeds"
-      feeds = self.feeds
-    else
-      # ensure folder belongs to the user
-      folder = self.folders.find folder_id
-      Rails.logger.info "User #{self.id} - #{self.email} is refreshing folder #{folder.id} - #{folder.title}"
-      feeds = folder.feeds
-    end
-
-    feeds.each {|feed| FeedClient.fetch feed.id}
-    entries = self.unread_folder_entries folder_id
-    return entries
-  end
-
-
 
   private
 
