6787d146792faabe29b4577938aeb5e9dc10d2a8
Alfredo Amatriain <geralt@gmail.com>
2013-05-22 14:30:22 +0200

    Changed the User.subscribe method. Moved the code that checks if the feed is already in the database to a new private method, User.subscribe_known_feed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -177,18 +177,11 @@ class User < ActiveRecord::Base
     # Ensure the url has a schema (defaults to http:// if none is passed)
     feed_url = ensure_schema url
 
-    # Check if there is a feed with that URL already in the database
-    known_feed = Feed.url_variants_feed feed_url
-    if known_feed.present?
-      # Check if the user is already subscribed to the feed
-      if self.feeds.include? known_feed
-        Rails.logger.info "User #{self.id} (#{self.email}) is already subscribed to feed #{known_feed.id} - #{known_feed.fetch_url}"
-        raise AlreadySubscribedError.new
-      end
-      Rails.logger.info "Subscribing user #{self.id} (#{self.email}) to pre-existing feed #{known_feed.id} - #{known_feed.fetch_url}"
-      self.feeds << known_feed
-      return known_feed
-    else
+    # Try to subscribe the user to the feed assuming it's in the database
+    feed = subscribe_known_feed feed_url
+
+    # If the feed is not in the database, save it and fetch it for the first time.
+    if feed.blank?
       Rails.logger.info "Feed #{feed_url} not in the database, trying to fetch it"
       feed = Feed.create! fetch_url: feed_url, title: feed_url
       fetch_result = FeedClient.fetch feed.id
@@ -197,13 +190,14 @@ class User < ActiveRecord::Base
         # We have to reload the feed because the title has likely changed value to the real one when first fetching it
         feed.reload
         self.feeds << feed
-        return feed
       else
         Rails.logger.info "URL #{feed_url} is not a valid feed URL"
         feed.destroy
-        return nil
+        feed = nil
       end
     end
+
+    return feed
   end
 
   ##
@@ -405,5 +399,34 @@ class User < ActiveRecord::Base
       fixed_url = url
     end
     return fixed_url
+  end
+
+  ##
+  # Subscribe the user to a feed already in the database.
+  #
+  # Receives as argument the URL of the feed. It can match the feed.url (normally the website URL) or
+  # the feed.fetch_url (the URL from which the feed is fetched).
+  #
+  # If the user is already subscribed to the feed, raises an AlreadySubscribedError.
+  #
+  # If there is no feed in the database matching the passed URL, returns nil.
+  #
+  # If a matching feed is found, the user is subscribed to it and the feed instance is returned.
+
+  def subscribe_known_feed(feed_url)
+    # Check if there is a feed with that URL already in the database
+    known_feed = Feed.url_variants_feed feed_url
+    if known_feed.present?
+      # Check if the user is already subscribed to the feed
+      if self.feeds.include? known_feed
+        Rails.logger.info "User #{self.id} (#{self.email}) is already subscribed to feed #{known_feed.id} - #{known_feed.fetch_url}"
+        raise AlreadySubscribedError.new
+      end
+      Rails.logger.info "Subscribing user #{self.id} (#{self.email}) to pre-existing feed #{known_feed.id} - #{known_feed.fetch_url}"
+      self.feeds << known_feed
+      return known_feed
+    else
+      return nil
+    end
   end
 end
