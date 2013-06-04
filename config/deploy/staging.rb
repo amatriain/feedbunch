f701be9f9c3aeddecc53dba62b580ad629cc46ea
Alfredo Amatriain <geralt@gmail.com>
2013-06-04 22:01:48 +0200

    Changed staging server for Capistrano deployment to staging.feedbunch.com
    However the SSH key to login into this server will never be stored in Github. It will be manually copied to development machines.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@
 #	Settings
 #############################################################
 
-server 'ec2-54-216-109-28.eu-west-1.compute.amazonaws.com', :app, :web, :db, primary: true
+server 'staging.feedbunch.com', :app, :web, :db, primary: true
 set :rails_env, :staging
