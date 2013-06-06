a62327ff2e95e9ff89e3c3b007efc25cc29ae8eb
Alfredo Amatriain <geralt@gmail.com>
2013-06-06 13:22:12 +0200

    Deployment in the production environment uses the server address "production.feedbunch.com"

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@
 #	Settings
 #############################################################
 
-server 'feedbunch.com', :app, :web, :db, primary: true
+server 'production.feedbunch.com', :app, :web, :db, primary: true
 ssh_options[:keys] = '/home/amatriain/Feedbunch/Production/Openreaderproduction.pem'
 set :rails_env, :production
