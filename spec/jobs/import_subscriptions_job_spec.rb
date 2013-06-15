e412b9372c960fb00a3eea70fac3d95a23948ef8
Alfredo Amatriain <geralt@gmail.com>
2013-06-15 23:09:29 +0200

    WIP: Background job to import a user's OPML file.

---
+++ spec/jobs/import_subscriptions_job_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe ImportSubscriptionsJob do
+
+  before :each do
+    @user = FactoryGirl.create :user
+    @data_import = FactoryGirl.build :data_import, user_id: @user.id, status: DataImport::RUNNING
+    @user.data_import = @data_import
+    @filename = File.join File.dirname(__FILE__), '..', 'attachments', '1371324422.opml'
+  end
+
+  it 'updates the data import total number of feeds'
+
+  it 'sets data import status to ERROR if the file does not exist'
+
+  it 'sets data import status to ERROR if the file is not valid XML'
+
+  it 'sets data import status to ERROR if the file is not valid OPML'
+
+  it 'does nothing if the user does not exist'
+
+  it 'subscribes user to already existing feeds'
+
+  it 'updates data import number of processed feeds when subscribing user to existing feeds'
+
+  it 'creates new feeds and subscribes user to them'
+
+  it 'enqueues job to fetch new feeds'
+
+  it 'creates folder structure'
+
+  it 'reuses folders already created by the user'
+
+  it 'does not update data import number of processed feeds when subscribing user to new feeds'
+
+  it 'sets data import status to SUCCESS if all feeds already existed'
+
+  it 'leaves data import status as RUNNING if there were new feeds'
+
+end
