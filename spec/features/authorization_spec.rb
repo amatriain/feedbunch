55cc2f394cbd0e886fbd591ca6be45e42ee7e922
Alfredo Amatriain <geralt@gmail.com>
2013-07-18 21:59:02 +0200

    Fixed acceptance test.
    During acceptance testing, when an admin user tries to access the resque-web page, an HTTP 500 will be returned because Resque and Redis are not running during testing. However it's enough to check that a RoutingError is not raised to check that the app is allowing access to the user based on his admin role.

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -23,8 +23,7 @@ describe 'authorization' do
 
   it 'allows access to Resque to admin users' do
     login_user_for_feature @admin_user
-    visit '/admin/resque'
-    page.status_code.should eq 200
+    expect {visit '/admin/resque'}.not_to raise_error ActionController::RoutingError
   end
 
   it 'does not allow access to Resque to non-admin users' do
