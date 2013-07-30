d30e41d0dbd4038a9c5d739129b820bbb51da26e
Alfredo Amatriain <geralt@gmail.com>
2013-07-30 22:36:34 +0200

    Added the background server to the Capistrano configuration for the production environment.
    Also use the right FQDN for the background server in the production environment, inside the Resque configuration file.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,6 @@
 #############################################################
 
 server 'production.feedbunch.com', :app, :web, :db, primary: true
+server 'background.production.feedbunch.com', :background
 ssh_options[:keys] = '/home/amatriain/Feedbunch/Production/Openreaderproduction.pem'
 set :rails_env, 'production'
