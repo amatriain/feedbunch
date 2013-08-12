174270220a3bb260a57bbceebb0fa2807c7e8998
Alfredo Amatriain <geralt@gmail.com>
2013-08-12 08:38:47 +0200

    Adapted db/schema.rb to ruby 1.9 hash syntax.

--- db/schema.rb
+++ db/schema.rb
@@ -9,20 +9,20 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130809065500) do
+ActiveRecord::Schema.define(version: 20130809065500) do
 
-  create_table "data_imports", :force => true do |t|
+  create_table "data_imports", force: true do |t|
     t.integer  "user_id"
     t.text     "status"
     t.integer  "total_feeds"
     t.integer  "processed_feeds"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  create_table "entries", :force => true do |t|
+  create_table "entries", force: true do |t|
     t.text     "title"
     t.text     "url"
     t.text     "author"
@@ -31,56 +31,56 @@ ActiveRecord::Schema.define(:version => 20130809065500) do
     t.datetime "published"
     t.text     "guid"
     t.integer  "feed_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "entry_states", :force => true do |t|
+  create_table "entry_states", force: true do |t|
     t.boolean  "read"
     t.integer  "user_id"
     t.integer  "entry_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "feed_subscriptions", :force => true do |t|
-    t.integer  "user_id",        :null => false
-    t.integer  "feed_id",        :null => false
+  create_table "feed_subscriptions", force: true do |t|
+    t.integer  "user_id",        null: false
+    t.integer  "feed_id",        null: false
     t.integer  "unread_entries"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
-  create_table "feeds", :force => true do |t|
+  create_table "feeds", force: true do |t|
     t.text     "title"
     t.text     "url"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.text     "fetch_url"
     t.text     "etag"
     t.text     "last_modified"
   end
 
-  create_table "feeds_folders", :force => true do |t|
+  create_table "feeds_folders", force: true do |t|
     t.integer "feed_id"
     t.integer "folder_id"
   end
 
-  create_table "folders", :force => true do |t|
+  create_table "folders", force: true do |t|
     t.integer  "user_id"
     t.text     "title"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "unread_entries"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -89,18 +89,18 @@ ActiveRecord::Schema.define(:version => 20130809065500) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        :default => 0
+    t.integer  "failed_attempts",        default: 0
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "admin",                  :default => false, :null => false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false, null: false
     t.integer  "unread_entries"
   end
 
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
 end
