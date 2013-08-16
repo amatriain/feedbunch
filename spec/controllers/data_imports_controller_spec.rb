6a9e8ade3d797efbd42c6cb3b0bcad9c7ec50ecc
Alfredo Amatriain <geralt@gmail.com>
2013-08-16 21:20:57 +0200

    Fixed unit test.

--- spec/controllers/data_imports_controller_spec.rb
+++ spec/controllers/data_imports_controller_spec.rb
@@ -26,13 +26,13 @@ describe DataImportsController do
 
     it 'redirects to feeds_path if successful' do
       User.any_instance.stub :import_subscriptions
-      post :create, data_imports: {file: 'mock_file'}
+      post :create, data_import: {file: 'mock_file'}
       response.should redirect_to feeds_path
     end
 
     it 'redirects to feeds_path if an error happens' do
       SubscriptionsImporter.stub(:read_data_file).and_raise StandardError.new
-      post :create, data_imports: {file: 'mock_file'}
+      post :create, data_import: {file: 'mock_file'}
       response.should redirect_to feeds_path
     end
   end
