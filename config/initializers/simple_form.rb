12b038db70c1672e7f11b38454a97ba9e187d581
Alfredo Amatriain <geralt@gmail.com>
2013-08-31 12:54:13 +0200

    Simplified Simple_form initialization.
    We only use one simple_form wrapper in all the app, which is supposed to generate markup compatible with bootstrap forms with prepended icons (even though it's not currently working because of the migration to Bootstrap 3). Removed from the app initializers all other wrapper definitions, which were unused clutter. Also set the only wrapper as the default one, so that each form does not have to explicitly declare which wrapper to use.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -5,48 +5,20 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, :class => :input,
-    :hint_class => :field_with_hint, :error_class => :field_with_errors do |b|
-    ## Extensions enabled by default
-    # Any of these extensions can be disabled for a
-    # given input by passing: `f.input EXTENSION_NAME => false`.
-    # You can make any of these extensions optional by
-    # renaming `b.use` to `b.optional`.
-
-    # Determines whether to use HTML5 (:email, :url, ...)
-    # and required attributes
+  config.wrappers :bootstrap3_prepend, :tag => 'div', :class => "control-group", :error_class => 'error' do |b|
     b.use :html5
-
-    # Calculates placeholders automatically from I18n
-    # You can also pass a string as f.input :placeholder => "Placeholder"
     b.use :placeholder
-
-    ## Optional extensions
-    # They are disabled unless you pass `f.input EXTENSION_NAME => :lookup`
-    # to the input. If so, they will retrieve the values from the model
-    # if any exists. If you want to enable the lookup for any of those
-    # extensions by default, you can change `b.optional` to `b.use`.
-
-    # Calculates maxlength from length validations for string inputs
-    b.optional :maxlength
-
-    # Calculates pattern from format validations for string inputs
-    b.optional :pattern
-
-    # Calculates min and max from length validations for numeric inputs
-    b.optional :min_max
-
-    # Calculates readonly automatically from readonly attributes
-    b.optional :readonly
-
-    ## Inputs
-    b.use :label_input
-    b.use :hint,  :wrap_with => { :tag => :span, :class => :hint }
-    b.use :error, :wrap_with => { :tag => :span, :class => :error }
+    b.use :label
+    b.wrapper :tag => 'div', :class => 'controls' do |input|
+      input.wrapper :tag => 'div', :class => 'input-prepend' do |prepend|
+        prepend.use :input
+      end
+      input.use :error, :wrap_with => { :tag => 'strong', :class => 'help-block' }
+      input.use :hint,  :wrap_with => { :tag => 'em', :class => 'help-block' }
+    end
   end
-
   # The default wrapper to be used by the FormBuilder.
-  #config.default_wrapper = :default
+  config.default_wrapper = :bootstrap3_prepend
 
   # Define the way to render check boxes / radio buttons with labels.
   # Defaults to :nested for bootstrap config.
