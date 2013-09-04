0bfec4029c5090425e868cf40dd43c431e2b384f
Alfredo Amatriain <geralt@gmail.com>
2013-09-04 19:13:20 +0200

    Moved the unsubscribe button to the navbar.

--- spec/features/feed_unsubscribe_spec.rb
+++ spec/features/feed_unsubscribe_spec.rb
@@ -17,14 +17,14 @@ describe 'unsubscribe from feed' do
 
   it 'hides unsubscribe button until a feed is selected', js: true do
     visit feeds_path
-    page.should have_css 'a#unsubscribe-feed.hidden', visible: false
+    page.should have_css '#unsubscribe-feed.hidden', visible: false
   end
 
   it 'shows unsubscribe button when a feed is selected', js: true do
     read_feed @feed1.id
     page.should_not have_css 'a#unsubscribe-feed.hidden', visible: false
     page.should_not have_css 'a#unsubscribe-feed.disabled', visible: false
-    page.should have_css 'a#unsubscribe-feed'
+    page.should have_css '#unsubscribe-feed'
   end
 
   it 'still shows buttons after unsubscribing from a feed', js: true do
@@ -45,8 +45,8 @@ describe 'unsubscribe from feed' do
 
   it 'hides unsubscribe button when reading a whole folder', js: true do
     read_feed 'all'
-    page.should have_css 'a#unsubscribe-feed.hidden', visible: false
-    page.should have_css 'a#unsubscribe-feed.disabled', visible: false
+    page.should have_css '#unsubscribe-feed.hidden', visible: false
+    page.should have_css '#unsubscribe-feed.disabled', visible: false
   end
 
   it 'shows a confirmation popup', js: true do
