023389997804f54ecf74aea24b3ce4c4559ee8ce
Alfredo Amatriain <geralt@gmail.com>
2013-11-23 00:42:40 +0100

    Acceptance test for Quick Reading mode should now pass either if the entry being scrutinized has become read or is in the process os becoming read.

--- spec/features/quick_reading_spec.rb
+++ spec/features/quick_reading_spec.rb
@@ -2,75 +2,80 @@ require 'spec_helper'
 
 describe 'quick reading mode' do
 
-  before :each do
-    @user = FactoryGirl.create :user
-    @feed = FactoryGirl.create :feed
+  context 'mark entries open when scrolling' do
 
-    @entries = []
-    (1..100).each do
-      entry = FactoryGirl.build :entry, feed_id: @feed.id
-      @feed.entries << entry
-      @entries << entry
+    before :each do
+      @user = FactoryGirl.create :user
+      @feed = FactoryGirl.create :feed
+
+      @entries = []
+      (1..100).each do
+        entry = FactoryGirl.build :entry, feed_id: @feed.id
+        @feed.entries << entry
+        @entries << entry
+      end
+
+      @user.subscribe @feed.fetch_url
+
+      login_user_for_feature @user
+
+      page.driver.resize_window(800, 600)
     end
 
-    @user.subscribe @feed.fetch_url
+    it 'does not enable quick reading mode by default', js: true do
+      # Quick Reading checkbox should not be checked in edit registration page
+      visit edit_user_registration_path
+      find('#user_quick_reading').should_not be_checked
 
-    login_user_for_feature @user
+      visit read_path
+      read_feed @feed, @user
 
-    page.driver.resize_window(800, 600)
-  end
+      # first entry of the list (the one created most recently) is unread
+      entry_should_be_marked_unread @entries[99]
 
-  it 'does not enable quick reading mode by default', js: true do
-    # Quick Reading checkbox should not be checked in edit registration page
-    visit edit_user_registration_path
-    find('#user_quick_reading').should_not be_checked
+      # scroll to bottom of page
+      page.execute_script 'window.scrollBy(0,10000)'
+      # wait 0.5 seconds for entries to be marked as read, if quick reading were enabled
+      sleep 0.5
+      # scroll to top of page again
+      page.execute_script 'window.scrollBy(0,-10000)'
 
-    visit read_path
-    read_feed @feed, @user
+      # first entry should still be unread
+      entry_should_be_marked_unread @entries[99]
+      # after refresh first entry should still be visible
+      read_feed @feed, @user
+      page.should have_text @entries[99].title
+    end
 
-    # first entry of the list (the one created most recently) is unread
-    entry_should_be_marked_unread @entries[99]
+    it 'enables quick reading mode', js: true do
+      enable_quick_reading @user
 
-    # scroll to bottom of page
-    page.execute_script 'window.scrollBy(0,10000)'
-    # wait 0.5 seconds for entries to be marked as read, if quick reading were enabled
-    sleep 0.5
-    # scroll to top of page again
-    page.execute_script 'window.scrollBy(0,-10000)'
+      # Quick Reading checkbox should be checked in edit registration page
+      visit edit_user_registration_path
+      find('#user_quick_reading').should be_checked
 
-    # first entry should still be unread
-    entry_should_be_marked_unread @entries[99]
-    # after refresh first entry should still be visible
-    read_feed @feed, @user
-    page.should have_text @entries[99].title
-  end
+      visit read_path
+      read_feed @feed, @user
 
-  it 'enables quick reading mode', js: true do
-    enable_quick_reading @user
+      # first entry of the list (the one created most recently) is unread
+      entry_should_be_marked_unread @entries[99]
 
-    # Quick Reading checkbox should be checked in edit registration page
-    visit edit_user_registration_path
-    find('#user_quick_reading').should be_checked
+      # scroll down enough to hide the top entry
+      page.execute_script "document.getElementById('entry-#{@entries[99].id}').scrollIntoView(true);"
+      page.execute_script 'window.scrollBy(0,50)'
+      # wait 0.5 seconds for entries to be marked as read, if quick reading were enabled
+      sleep 0.5
+      # scroll to top of page again
+      page.execute_script "document.getElementById('entry-#{@entries[99].id}').scrollIntoView(true);"
 
-    visit read_path
-    read_feed @feed, @user
-
-    # first entry of the list (the one created most recently) is unread
-    entry_should_be_marked_unread @entries[99]
-
-    # scroll down enough to hide the top entry
-    page.execute_script "document.getElementById('entry-#{@entries[99].id}').scrollIntoView(true);"
-    page.execute_script 'window.scrollBy(0,50)'
-    # wait 0.5 seconds for entries to be marked as read, if quick reading were enabled
-    sleep 0.5
-    # scroll to top of page again
-    page.execute_script "document.getElementById('entry-#{@entries[99].id}').scrollIntoView(true);"
-
-    # first entry should be read
-    page.should have_css "a[data-entry-id='#{@entries[99].id}'].entry-read"
-    # after refresh first entry should not be visible
-    read_feed @feed, @user
-    page.should_not have_text @entries[99].title
+      # first entry should be read
+      entry_read = page.has_css? "a[data-entry-id='#{@entries[99].id}'].entry-read"
+      entry_becoming_read = page.has_css? "a[data-entry-id='#{@entries[99].id}'].entry-becoming-read"
+      (entry_read || entry_becoming_read).should be_true
+      # after refresh first entry should not be visible
+      read_feed @feed, @user
+      page.should_not have_text @entries[99].title
+    end
   end
 
 end
