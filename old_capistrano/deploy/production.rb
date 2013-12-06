19be575d3b4ce6ea3d9f69fedaf5ebd2e0b84012
Alfredo Amatriain <geralt@gmail.com>
2013-12-06 12:54:38 +0100

    Removed old capistrano 2.x config files.
    They are no longer needed, transition has been completed to capistrano 3.

--- old_capistrano/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-#############################################################
-#	Settings
-#############################################################
-
-server 'production.feedbunch.com', :app, :web, :db, primary: true
-server 'background.production.feedbunch.com', :background
-ssh_options[:keys] = '/home/amatriain/Feedbunch/Production/Openreaderproduction.pem'
-set :rails_env, 'production'
--- old_capistrano/deploy/staging.rb
+++
@@ -1,8 +0,0 @@
-#############################################################
-#	Settings
-#############################################################
-
-server 'staging.feedbunch.com', :app, :web, :db, primary: true
-server 'background.staging.feedbunch.com', :background
-ssh_options[:keys] = '/home/amatriain/Feedbunch/Staging/Openreaderstaging.pem'
-set :rails_env, 'staging'
