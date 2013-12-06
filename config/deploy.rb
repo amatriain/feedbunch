d9208854d82f282552f60574e6a983f2b4542ccf
Alfredo Amatriain <geralt@gmail.com>
2013-12-06 22:45:42 +0100

    Refactored capistrano script to use the new "within" and "with" directives.
    Clearer code now, without having to manually run "cd" bash commands in the servers.

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,20 +63,24 @@ namespace :feedbunch_god do
   desc 'Start God-managed tasks: Redis, Resque'
   task :start do
     on roles :background do
-      execute "cd #{current_path};",
-              "RAILS_ENV=#{fetch(:rails_env)} RESQUE_ENV=background bundle exec god -c #{File.join(current_path,'config','background_jobs.god')} --log #{shared_path}/log/god.log"
+      within fetch(:current_path) do
+        with rails_env: fetch(:rails_env) do
+          execute "RESQUE_ENV=background bundle exec god -c #{File.join(current_path,'config','background_jobs.god')} --log #{shared_path}/log/god.log"
+        end
+      end
     end
   end
 
   desc 'Stop God-managed tasks: Redis, Resque'
   task :stop do
     on roles :background do
-      # We run a "true" shell command after issuing a "god terminate" command because otherwise if
-      # God were not running before this, we would get a return value of false which
-      # Capistrano would intepret as an error and the deployment would be rolled back
-      execute "cd #{current_path};",
-              'bundle exec god terminate;',
-              'true'
+      within fetch(:current_path) do
+        # We run a "true" shell command after issuing a "god terminate" command because otherwise if
+        # God were not running before this, we would get a return value of false which
+        # Capistrano would intepret as an error and the deployment would be rolled back
+        execute 'bundle exec god terminate;',
+                'true'
+      end
     end
   end
 
