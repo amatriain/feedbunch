ee5ba2576518fdcf0dff5d0258bee56edf0000c0
Alfredo Amatriain <geralt@gmail.com>
2013-12-13 21:17:35 +0100

    When trimming text fields in already existing entries, if this would result in a duplicate entry (which would raise an error when trying to save in the database) keep one of the two database records.

--- db/migrate/20131213124416_trim_entry_strings.rb
+++ db/migrate/20131213124416_trim_entry_strings.rb
@@ -1,13 +1,20 @@
 class TrimEntryStrings < ActiveRecord::Migration
   def change
     Entry.all.each do |entry|
-      entry.title = entry.title.try :strip
-      entry.url = entry.url.try :strip
-      entry.author = entry.author.try :strip
-      entry.content = entry.content.try :strip
-      entry.summary = entry.summary.try :strip
-      entry.guid = entry.guid.try :strip
-      entry.save!
+      begin
+        entry.title = entry.title.try :strip
+        entry.url = entry.url.try :strip
+        entry.author = entry.author.try :strip
+        entry.content = entry.content.try :strip
+        entry.summary = entry.summary.try :strip
+        entry.guid = entry.guid.try :strip
+        entry.save!
+      rescue
+        # If a duplicate entry is found when trimming, keep one of them
+        if Entry.exists? feed_id: entry.id, guid: entry.guid.try(:strip)
+          entry.destroy
+        end
+      end
     end
   end
 end
