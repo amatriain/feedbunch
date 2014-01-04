5d8a2181f7bd1f5a6947ccb4b0dbc2bc07839902
Alfredo Amatriain <geralt@gmail.com>
2014-01-04 10:33:09 +0100

    Silence a warning related to new I18n behavior.
    When an unavailable locale is passed, I really don't want an error to be raised. That's what the fallback to :en is for.
    
    For more detail see:
    
      http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Feedbunch
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # Do not raise an error if an unavailable locale is passed (the default :en will be used,
+    # see config.i18n.fallbacks below)
+    config.i18n.enforce_available_locales = false
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
