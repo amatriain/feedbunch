4cf8fa2984e1530cc640f41ffc93ee874aed9a03
Alfredo Amatriain <geralt@gmail.com>
2014-01-09 11:55:26 +0100

    Change migration to avoid setting a "not null" constraint on the name column while some records still have a null value there.
    This should fix a MySQL error while deploying.

--- db/migrate/20140108210438_change_name_not_null_in_users.rb
+++ db/migrate/20140108210438_change_name_not_null_in_users.rb
@@ -1,5 +1,8 @@
 class ChangeNameNotNullInUsers < ActiveRecord::Migration
   def up
+    User.where(name: nil).each do |user|
+      user.update name: user.email
+    end
     change_column :users, :name, :text, null: false
   end
 
