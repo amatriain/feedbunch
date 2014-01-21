34319c2f0b0613a28d144873997cbc5284f99119
Alfredo Amatriain <geralt@gmail.com>
2014-01-21 13:03:04 +0100

    Removed old example comment from deployment script. Explicitly tell capistrano to restart the Passenger app after deploying it.
    From Capistrano 3.1 onwards the app is not automatically restarted after redeploying. It's necessary to explicitly include a restart step after the publish step if we want the app to be restarted every time (which we do for this app).
    
    For more details see the Capistrano 3.1.0 changelog:
    
      https://github.com/capistrano/capistrano/blob/master/CHANGELOG.md

--- config/deploy.rb
+++ config/deploy.rb
@@ -146,33 +146,9 @@ namespace :deploy do
     invoke 'feedbunch_god:restart_resque'
   end
 
+  # after deploying, restart the Passenger app
+  after 'deploy:publishing', 'deploy:restart'
+
   # clean up old releases on each deploy, keep only 5 most recent releases
   after 'deploy:restart', 'deploy:cleanup'
 end
-
-
-
-# Task definition example:
-#
-#namespace :deploy do
-#
-#  desc 'Restart application'
-#  task :restart do
-#    on roles(:app), in: :sequence, wait: 5 do
-#      # Your restart mechanism here, for example:
-#      # execute :touch, release_path.join('tmp/restart.txt')
-#    end
-#  end
-#
-#  after :restart, :clear_cache do
-#    on roles(:web), in: :groups, limit: 3, wait: 10 do
-#      # Here we can do anything such as:
-#      # within release_path do
-#      #   execute :rake, 'cache:clear'
-#      # end
-#    end
-#  end
-#
-#  after :finishing, 'deploy:cleanup'
-#
-#end
