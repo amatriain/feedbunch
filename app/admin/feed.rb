6dac3f8721b6113aa95247b32e91c7beb41cee3e
Alfredo Amatriain <geralt@gmail.com>
2014-02-11 15:46:23 +0100

    Customized ActiveAdmin views for Feeds and Users.

--- app/admin/feed.rb
+++ app/admin/feed.rb
@@ -1,17 +1,33 @@
 ActiveAdmin.register Feed do
+  permit_params :title, :url, :fetch_url, :available, :fetch_interval_secs
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  index do
+    column :title
+    column :url
+    column :fetch_url
+    column :available
+    column :last_fetched
+    column :fetch_interval_secs
+    column :failing_since
+    column :etag
+    column :last_modified
+    default_actions
+  end
+
+  filter :title
+  filter :url
+  filter :fetch_url
+  filter :available
+
+  form do |f|
+    f.inputs 'Feed Details' do
+      f.input :title
+      f.input :url
+      f.input :fetch_url
+      f.input :available
+      f.input :fetch_interval_secs
+    end
+    f.actions
+  end
   
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,21 +1,28 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :name, :admin
 
   index do
     column :email
+    column :name
     column :current_sign_in_at
     column :last_sign_in_at
     column :sign_in_count
+    column :admin
+    column :locale
+    column :timezone
+    column :quick_reading
+    column :open_all_entries
     default_actions
   end
 
   filter :email
+  filter :name
 
   form do |f|
-    f.inputs "Admin Details" do
+    f.inputs 'User Details' do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :name
+      f.input :admin
     end
     f.actions
   end
