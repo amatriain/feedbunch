4ce2a659c1cdac2f92931c44bdbd1353f1f87686
Alfredo Amatriain <geralt@gmail.com>
2014-02-12 20:50:26 +0100

    Added dashboard page to ActiveAdmin with recent relevant information.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,70 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  menu :priority => 1, :label => proc{ I18n.t('active_admin.dashboard') }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content :title => proc{ I18n.t('active_admin.dashboard') } do
+
+    columns do
+      column do
+        panel 'General info' do
+          ul do
+            li "#{User.count} users"
+            li "#{Feed.count} feeds"
+            li "#{Entry.count} entries"
+            li "#{Folder.count} folders"
+          end
+        end
+      end
+      column do
+        panel 'Failing feeds info' do
+          ul do
+            li "#{Feed.where(available: true).where.not(failing_since: nil).count} feeds currently failing"
+            li "#{Feed.where(available: false).count} permanently unavailable feeds"
+          end
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel 'Recently failing feeds' do
+          ul do
+            Feed.where(available: true).where.not(failing_since: nil).order('created_at DESC').limit(10).map do |feed|
+              li link_to("#{feed.title} (#{feed.fetch_url})", admin_feed_path(feed))
+            end
+          end
+        end
+      end
+      column do
+        panel 'Recent permanently unavailable feeds' do
+          ul do
+            Feed.where(available: false).order('updated_at DESC').limit(10).map do |feed|
+              li link_to("#{feed.title} (#{feed.fetch_url})", admin_feed_path(feed))
+            end
+          end
+        end
+      end
+    end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    columns do
+      column do
+       panel 'Recent users' do
+         ul do
+           User.order('created_at DESC').limit(10).map do |user|
+             li link_to("#{user.name} (#{user.email})", admin_user_path(user))
+           end
+         end
+       end
+      end
+      column do
+        panel 'Recently added feeds' do
+          ul do
+            Feed.order('created_at DESC').limit(10).map do |feed|
+              li link_to("#{feed.title} (#{feed.fetch_url})", admin_feed_path(feed))
+            end
+          end
+        end
+      end
+    end
+  end
 end
