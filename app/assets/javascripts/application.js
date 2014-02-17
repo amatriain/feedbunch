bcbc232a6cc64e3c5cfe57a9e2488aacfd863e16
Alfredo Amatriain <geralt@gmail.com>
2014-02-17 17:50:46 +0100

    Removed jquery-unveil JS library.
    Lazy loading of images will be achieved with a custom angularjs service.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,3 @@
 //= require ../../../vendor/assets/javascripts/ng-infinite-scroll.min
 //= require ../../../vendor/assets/javascripts/withinViewport
 //= require ../../../vendor/assets/javascripts/jquery.withinViewport
-//= require ../../../vendor/assets/javascripts/jquery.unveil.min
