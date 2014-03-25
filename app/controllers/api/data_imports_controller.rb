amatriain/feedbunch

commit 264d9a2f84437ae7788ce1dfb68b751782bfb12e

Changed all routes which accept or return JSON to a named "api" subpath.
This means instead of /feeds.json, now /api/feeds.json will be invoked.

This means a change in controllers, views, routes and most javascript that uses $http to send and receive JSON via AJAX.

This way project has better maintainability. E.g. all views that generate JSON and are meant to be invoked via AJAX are under a /app/views/api folder. Also this would enable us in the future to support a new version of the API e.g. in the "apiv2" subpath without removing the current API.

app/controllers/api/data_imports_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to import and export subscriptions data
 
-class DataImportsController < ApplicationController
+class API::DataImportsController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/entries_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to access the Feed model.
 
-class EntriesController < ApplicationController
+class API::EntriesController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/feeds_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to access the Feed model.
 
-class FeedsController < ApplicationController
+class API::FeedsController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/folders_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to access the Folder model.
 
-class FoldersController < ApplicationController
+class API::FoldersController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/user_data_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to retrieve data for the current user
 
-class UserDataController < ApplicationController
+class API::UserDataController < ApplicationController
 
   before_filter :authenticate_user!
 

spec/controllers/data_imports_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe DataImportsController do
+describe API::DataImportsController do
 
   before :each do
     @user = FactoryGirl.create :user

commit d369d05fe62a493bc000af3f38153ca3a763ccea

Fixed several errors introduced in 264d9a2f84437ae7788ce1dfb68b751782bfb12e:
- the namespace for API controllers cannot be "API::". It has to be "Api::". I've found no way around rails default capitalization rules for this.

- in feeds and folders show views, a reference to the entries show view has been updated so that the view is found in its new location under the "api" folder.

app/controllers/api/data_imports_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to import and export subscriptions data
 
-class API::DataImportsController < ApplicationController
+class Api::DataImportsController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/entries_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to access the Feed model.
 
-class API::EntriesController < ApplicationController
+class Api::EntriesController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/feeds_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to access the Feed model.
 
-class API::FeedsController < ApplicationController
+class Api::FeedsController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/folders_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to access the Folder model.
 
-class API::FoldersController < ApplicationController
+class Api::FoldersController < ApplicationController
 
   before_filter :authenticate_user!
 

app/controllers/api/user_data_controller.rb
@@ -1,7 +1,7 @@
 ##
 # Controller to retrieve data for the current user
 
-class API::UserDataController < ApplicationController
+class Api::UserDataController < ApplicationController
 
   before_filter :authenticate_user!
 

app/views/api/feeds/show.json.erb
@@ -2,7 +2,7 @@
 "unread_entries": <%= user.feed_unread_count feed %>,
 "entries": [
 <% entries.each do |entry| %>
-<%= render partial: 'entries/entry', locals: {entry: entry, user: user} %>
+<%= render partial: 'api/entries/entry', locals: {entry: entry, user: user} %>
   <% if entry != entries.last %>
 ,
   <% end %>

app/views/api/folders/show.json.erb
@@ -1,7 +1,7 @@
 {
   "entries": [
     <% entries.each do |entry| %>
-    <%= render partial: 'entries/entry', locals: {entry: entry, user: user} %>
+    <%= render partial: 'api/entries/entry', locals: {entry: entry, user: user} %>
     <% if entry != entries.last %>
     ,
     <% end %>

spec/controllers/data_imports_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe API::DataImportsController do
+describe Api::DataImportsController do
 
   before :each do
     @user = FactoryGirl.create :user

spec/controllers/entries_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe API::EntriesController do
+describe Api::EntriesController do
 
   before :each do
     @feed = FactoryGirl.create :feed

spec/controllers/feeds_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe API::FeedsController do
+describe Api::FeedsController do
 
   before :each do
     @user = FactoryGirl.create :user

spec/controllers/folders_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe API::FoldersController do
+describe Api::FoldersController do
 
   before :each do
     @user = FactoryGirl.create :user

spec/controllers/user_data_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe API::UserDataController do
+describe Api::UserDataController do
 
   before :each do
     @user = FactoryGirl.create :user