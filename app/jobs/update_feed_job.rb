530be019977466568b7a1ee85de5c02983479c80
Alfredo Amatriain <geralt@gmail.com>
2014-03-28 19:12:04 +0100

    Moved the code that updates a feed to a new class lib/feed_updater. UpdateFeedJob just invokes this class.
    This enables us to create a different job for when the update is manually requested by a user. This job will receive as an additional argument the id of the user, will also invoke lib/feed_updater and additionally will update the status of the job in the database, so that the user can see this status in the view.

--- app/jobs/update_feed_job.rb
+++ app/jobs/update_feed_job.rb
@@ -1,5 +1,6 @@
 require 'subscriptions_manager'
 require 'schedule_manager'
+require 'feed_updater'
 
 ##
 # Background job to fetch and update a feed's entries.
@@ -13,71 +14,9 @@ class UpdateFeedJob
   # Fetch and update entries for the passed feed.
   # Receives as argument the id of the feed to be fetched.
   #
-  # If the feed does not exist, further refreshes of the feed are unscheduled. This avoids the case
-  # in which scheduled updates for a deleted feed happened periodically.
-  #
-  # Every time a feed update runs the unread entries count for each subscribed user are recalculated and corrected if necessary
-  #
   # This method is intended to be invoked from Resque, which means it is performed in the background.
 
   def self.perform(feed_id)
-    # Check that feed actually exists
-    if !Feed.exists? feed_id
-      Rails.logger.warn "Feed #{feed_id} scheduled to be updated, but it does not exist in the database. Unscheduling further updates."
-      ScheduleManager.unschedule_feed_updates feed_id
-      return
-    end
-    feed = Feed.find feed_id
-
-    Rails.logger.debug "Updating feed #{feed.id} - #{feed.title}"
-
-    # Initialize the number of entries in the feed before and after fetching, so the variables can be
-    # used in the ensure clause even if an error is raised while fetching (e.g. the server responds
-    # with a HTTP error code)
-    entries_before = feed.entries.count
-    entries_after = 0
-
-    # Fetch feed
-    FeedClient.fetch feed
-
-    entries_after = feed.entries.count
-
-    # If the update didn't fail, mark the feed as "not currently failing"
-    feed.update failing_since: nil if !feed.failing_since.nil?
-
-  rescue RestClient::Exception, SocketError, Errno::ETIMEDOUT, Errno::ECONNREFUSED, EmptyResponseError, FeedAutodiscoveryError, FeedFetchError, FeedParseError => e
-    # all these errors mean the feed cannot be updated, but the job itself has not failed. Do not re-raise the error
-    if feed.present?
-      # If this is the first update that fails, save the date&time the feed started failing
-      feed.update failing_since: Time.zone.now if feed.failing_since.nil?
-
-      # If the feed has been failing for too long, mark it as unavailable
-      if Time.zone.now - feed.failing_since > Feedbunch::Application.config.unavailable_after
-        feed.update available: false
-      end
-    end
-
-    Rails.logger.error "Error fetching feed #{feed_id} - #{feed.try :fetch_url}"
-    Rails.logger.error e.message
-    Rails.logger.error e.backtrace
-  ensure
-    if feed.present?
-      # Update timestamp of the last time the feed was fetched
-      Rails.logger.debug "Updating time of last update for feed #{feed.id} - #{feed.title}"
-      feed.update! last_fetched: Time.zone.now
-
-      if entries_after > entries_before
-        # If new entries have been fetched, decrement the fetch interval
-        ScheduleManager.decrement_update_interval feed
-      else
-        # If no new entries have been fetched, increment the fetch interval
-        ScheduleManager.increment_update_interval feed
-      end
-
-      # Update unread entries count for all subscribed users.
-      feed.users.each do |user|
-        SubscriptionsManager.recalculate_unread_count feed, user
-      end
-    end
+    FeedUpdater.update_feed feed_id
   end
 end
