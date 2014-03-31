d4104a7d6bcaac8c9fade854c2eafee42ef526c2
Alfredo Amatriain <geralt@gmail.com>
2014-03-31 14:38:23 +0200

    Finished implementing background job for user-requested feed refreshes.
    This job can mark a feed as "available" and "not currently failing" if the update is successful, same as the scheduled updates. However it does not change the schedule itself; perhaps in the future this could be desirable, but right now it's too complex and would need further thought.

--- app/jobs/refresh_feed_job.rb
+++ app/jobs/refresh_feed_job.rb
@@ -1,5 +1,3 @@
-require 'feed_updater'
-
 ##
 # Background job for user-requested updates to a feed.
 #
@@ -9,27 +7,77 @@ class RefreshFeedJob
   @queue = :update_feeds
 
   ##
-  # Fetch and update entries for the passed feed.
-  # Receives as argument the id of the feed to be fetched.
+  # Fetch and update entries for a feed, as requested by a user.
+  # Receives as argument the id of the refresh_feed_job_status associated with this refresh.
   #
   # This method is intended to be invoked from Resque, which means it is performed in the background.
 
-  def self.perform(user_id, feed_id)
-    # TODO Change arguments to a single refresh_feed_job_status_id !!!!!
+  def self.perform(refresh_feed_job_status_id)
+    # Check that refresh_feed_job_status actually exists
+    if !RefreshFeedJobStatus.exists? refresh_feed_job_status_id
+      Rails.logger.warn "Processing RefreshFeedJob for refresh_feed_job_status #{refresh_feed_job_status_id} but that status does not exist in the database. Aborting"
+      return
+    end
+    job_status = RefreshFeedJobStatus.find refresh_feed_job_status_id
+
+    # Check that the refresh_job_status is in state "RUNNING"
+    if job_status.status != RefreshFeedJobStatus::RUNNING
+      Rails.logger.warn "Processing RefreshFeedJob for refresh_feed_job_status #{job_status.id}, it should be in state RUNNING but it is in status #{job_status.status}. Aborting."
+      return
+    end
 
     # Check that user actually exists
-    if !User.exists? user_id
-      Rails.logger.warn "User #{user_id} requested refresh of feed #{feed_id}, but the user does not exist in the database."
+    if !User.exists? job_status.user_id
+      Rails.logger.warn "User #{job_status.user_id} requested refresh of feed #{job_status.feed_id}, but the user does not exist in the database. Aborting"
+      job_status.destroy
       return
     end
-    user = User.find user_id
+    user = User.find job_status.user_id
 
     # Check that user is subscribed to the feed
-    if !user.feeds.exists? feed_id
-      Rails.logger.warn "User #{user_id} requested refresh of feed #{feed_id}, but the user is not subscribed to the feed."
+    if !user.feeds.exists? job_status.feed_id
+      Rails.logger.warn "User #{user.id} requested refresh of feed #{job_status.feed_id}, but the user is not subscribed to the feed. Aborting."
+      job_status.destroy
       return
     end
 
-    FeedUpdater.update_feed feed_id
+    # Check that feed actually exists
+    if !Feed.exists? job_status.feed_id
+      Rails.logger.warn "Feed #{job_status.feed_id} scheduled to be updated, but it does not exist in the database. Aborting."
+      job_status.destroy
+      return
+    end
+    feed = Feed.find job_status.feed_id
+
+    # Fetch feed
+    Rails.logger.debug "Refreshing feed #{feed.id} - #{feed.title}"
+    FeedClient.fetch feed
+
+    Rails.logger.debug "Successfully finished refresh_feed_job_status #{refresh_feed_job_status_id} for feed #{feed.try :id}, user #{user.try :id}"
+    job_status.update status: RefreshFeedJobStatus::SUCCESS
+
+    # If the update didn't fail, mark the feed as "not currently failing" and "available"
+    feed.update failing_since: nil if !feed.failing_since.nil?
+    feed.update available: true if !feed.available
+
+  rescue RestClient::Exception, SocketError, Errno::ETIMEDOUT, Errno::ECONNREFUSED, EmptyResponseError, FeedAutodiscoveryError, FeedFetchError, FeedParseError => e
+    # all these errors mean the feed cannot be updated, but the job itself has not failed. Do not re-raise the error
+    Rails.logger.error "Error running refresh_feed_job_status #{refresh_feed_job_status_id} for feed #{feed.try :id}, user #{user.try :id}"
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace
+    if job_status.present?
+      job_status.update status: RefreshFeedJobStatus::ERROR
+    end
+  ensure
+    if feed.present?
+      # Update timestamp of the last time the feed was fetched
+      Rails.logger.debug "Updating time of last update for feed #{feed.id} - #{feed.title}"
+      feed.update! last_fetched: Time.zone.now
+
+      # Update unread entries count for all subscribed users.
+      feed.users.each do |user|
+        SubscriptionsManager.recalculate_unread_count feed, user
+      end
+    end
   end
 end
--- spec/jobs/refresh_feed_job_spec.rb
+++ spec/jobs/refresh_feed_job_spec.rb
@@ -6,13 +6,14 @@ describe RefreshFeedJob do
     @user = FactoryGirl.create :user
     @feed = FactoryGirl.create :feed
     @user.subscribe @feed.fetch_url
+    @refresh_feed_job_status = FactoryGirl.create :refresh_feed_job_status, user_id: @user.id, feed_id: @feed.id
     FeedClient.stub :fetch
   end
 
   it 'updates feed when the job runs' do
     FeedClient.should_receive(:fetch).with @feed
 
-    RefreshFeedJob.perform @user.id, @feed.id
+    RefreshFeedJob.perform @refresh_feed_job_status.id
   end
 
   it 'recalculates unread entries count in feed' do
@@ -28,28 +29,20 @@ describe RefreshFeedJob do
     feed_subscription = FeedSubscription.where(user_id: user.id, feed_id: @feed.id).first
     feed_subscription.update unread_entries: 10
 
-    RefreshFeedJob.perform @user.id, @feed.id
+    RefreshFeedJob.perform @refresh_feed_job_status.id
 
     # Unread count should be corrected
     user.feed_unread_count(@feed).should eq 1
   end
 
-  it 'unschedules updates if the feed has been deleted when the job runs' do
-    @feed.destroy
-    Resque.should_receive(:remove_schedule).with "update_feed_#{@feed.id}"
-    FeedClient.should_not_receive :fetch
+  context 'validations' do
 
-    RefreshFeedJob.perform @user.id, @feed.id
-  end
+    it 'does not update feed if the refresh_feed_job_status does not exist' do
+      @refresh_feed_job_status.destroy
+      FeedClient.should_not_receive :fetch
 
-  it 'does not update feed if it has been deleted' do
-    FeedClient.should_not_receive :fetch
-    @feed.destroy
-
-    RefreshFeedJob.perform @user.id, @feed.id
-  end
-
-  context 'update refresh_feed_job_status' do
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+    end
 
     it 'does not update feed if the user does not exist' do
       # subscribe a second user to the feed so that it is not destroyed when @user unsubscribes
@@ -58,274 +51,76 @@ describe RefreshFeedJob do
       @user.destroy
       FeedClient.should_not_receive :fetch
 
-      RefreshFeedJob.perform @user.id, @feed.id
+      RefreshFeedJob.perform @refresh_feed_job_status.id
     end
 
-    it 'does not update feed if the user is not subscribed' do
-      user2 = FactoryGirl.create :user
+    it 'destroys refresh_feed_job_status if the user does not exist' do
+      @user.delete
       FeedClient.should_not_receive :fetch
 
-      RefreshFeedJob.perform user2.id, @feed.id
+      RefreshFeedJobStatus.count.should eq 1
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+      RefreshFeedJobStatus.count.should eq 0
     end
 
-    it 'creates refresh_feed_job_status with status RUNNING if the user has none' do
-      #PENDING
-      @user.refresh_feed_job_statuses.destroy_all
-      @user.refresh_feed_job_statuses.should be_blank
+    it 'does not update feed if it does not exist' do
+      FeedClient.should_not_receive :fetch
+      @feed.destroy
 
-      RefreshFeedJob.perform @user.id, @feed.id
-      @user.refresh_feed_job_statuses.should_not be_blank
-      job_status = @user.refresh_feed_job_statuses.first
-      job_status.user_id.should eq @user.id
-      job_status.feed_id.should eq @feed.id
-      job_status.status.should eq RefreshFeedJobStatus::RUNNING
+      RefreshFeedJob.perform @refresh_feed_job_status.id
     end
 
-    it 'does not update feed if refresh_feed_job_status is not RUNNING'
+    it 'destroys refresh_feed_job_status if the feed does not exist' do
+      @feed.delete
+      FeedClient.should_not_receive :fetch
 
-    it 'updates refresh_feed_job_status to SUCCESS if successful'
+      RefreshFeedJobStatus.count.should eq 1
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+      RefreshFeedJobStatus.count.should eq 0
+    end
 
-    it 'updates refresh_feed_job_status to ERROR if an error is raised'
+
+    it 'does not update feed if the user is not subscribed' do
+      FeedSubscription.where(user_id: @user.id, feed_id: @feed.id).first.delete
+      FeedClient.should_not_receive :fetch
+
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+    end
+
+    it 'destroys refresh_feed_job_status if the user is not subscribed' do
+      FeedSubscription.where(user_id: @user.id, feed_id: @feed.id).first.delete
+      FeedClient.should_not_receive :fetch
+
+      RefreshFeedJobStatus.count.should eq 1
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+      RefreshFeedJobStatus.count.should eq 0
+    end
   end
 
-  context 'adaptative schedule' do
+  context 'update refresh_feed_job_status' do
 
-    it 'updates the last_fetched timestamp of the feed when successful' do
-      date = Time.zone.parse '2000-01-01'
-      ActiveSupport::TimeZone.any_instance.stub(:now).and_return date
+    it 'does not update feed if refresh_feed_job_status is not RUNNING' do
+      @refresh_feed_job_status.update status: RefreshFeedJobStatus::SUCCESS
+      FeedClient.should_not_receive :fetch
 
-      @feed.last_fetched.should be_nil
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.last_fetched.should eq date
+      RefreshFeedJob.perform @refresh_feed_job_status.id
     end
 
-    it 'decrements a 10% the fetch interval if new entries are fetched' do
-      FeedClient.stub(:fetch) do
-        entry = FactoryGirl.build :entry, feed_id: @feed.id
-        @feed.entries << entry
-      end
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3240s'
-        config[:every][1].should eq ({first_in: 3240})
-      end
-
-      @feed.reload.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3240
+    it 'updates refresh_feed_job_status to SUCCESS if successful' do
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+      @refresh_feed_job_status.reload.status.should eq RefreshFeedJobStatus::SUCCESS
     end
 
-    it 'increments a 10% the fetch interval if no new entries are fetched' do
-      FeedClient.stub(:fetch)
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.reload.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
-    it 'does not set a fetch interval smaller than the configured minimum' do
-      FeedClient.stub(:fetch) do
-        entry = FactoryGirl.build :entry, feed_id: @feed.id
-        @feed.entries << entry
-      end
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '900s'
-        config[:every][1].should eq ({first_in: 900.seconds})
-      end
-
-      @feed.update fetch_interval_secs: 15.minutes
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 15.minutes
-    end
-
-    it 'does not set a fetch interval greater than the configured maximum' do
-      FeedClient.stub(:fetch)
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '43200s'
-        config[:every][1].should eq ({first_in: 12.hours})
-      end
-
-      @feed.update fetch_interval_secs: 12.hours
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 12.hours
-    end
-
-  end
-
-  context 'error handling' do
-
-    it 'increments the fetch interval if the feed server returns an HTTP error status' do
+    it 'updates refresh_feed_job_status to ERROR if an error is raised when fetching feed' do
       FeedClient.stub(:fetch).and_raise RestClient::Exception.new
 
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
+      @refresh_feed_job_status.reload.status.should eq RefreshFeedJobStatus::RUNNING
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+      @refresh_feed_job_status.reload.status.should eq RefreshFeedJobStatus::ERROR
     end
-
-    it 'increments the fetch interval if the feed server FQDN cannot be resolved' do
-      FeedClient.stub(:fetch).and_raise SocketError.new('getaddrinfo: Name or service not known')
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
-    it 'increments the fetch interval if the feed server connection times out' do
-      FeedClient.stub(:fetch).and_raise Errno::ETIMEDOUT.new('Connection timed out')
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
-    it 'increments the fetch interval if the server refuses the connection' do
-      FeedClient.stub(:fetch).and_raise Errno::ECONNREFUSED.new('Connection refused - connect(2) for "feed.com" port 80')
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
-    it 'increments the fetch interval if the feed server response is empty' do
-      FeedClient.stub(:fetch).and_raise EmptyResponseError.new
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
-    it 'increments the fetch interval if there is a problem trying to do a feed autodiscovery' do
-      FeedClient.stub(:fetch).and_raise FeedAutodiscoveryError.new
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
-    it 'increments the fetch interval if there is a problem trying to fetch a valid feed xml' do
-      FeedClient.stub(:fetch).and_raise FeedFetchError.new
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
-    it 'increments the fetch interval if there is a problem trying to parse the xml response' do
-      FeedClient.stub(:fetch).and_raise FeedParseError.new
-
-      Resque.should_receive :set_schedule do |name, config|
-        name.should eq "update_feed_#{@feed.id}"
-        config[:class].should eq 'ScheduledUpdateFeedJob'
-        config[:persist].should be_true
-        config[:args].should eq @feed.id
-        config[:every][0].should eq '3960s'
-        config[:every][1].should eq ({first_in: 3960})
-      end
-
-      @feed.fetch_interval_secs.should eq 3600
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.fetch_interval_secs.should eq 3960
-    end
-
   end
 
   context 'failing feed' do
-
-    it 'sets failing_since to the current date&time the first time an update fails' do
-      FeedClient.stub(:fetch).and_raise RestClient::Exception.new
-      date = Time.zone.parse '2000-01-01'
-      ActiveSupport::TimeZone.any_instance.stub(:now).and_return date
-
-      @feed.failing_since.should be_nil
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.failing_since.should eq date
-    end
 
     it 'sets failing_since to nil when an update runs successfully' do
       FeedClient.stub(:fetch)
@@ -333,11 +128,19 @@ describe RefreshFeedJob do
       @feed.update failing_since: date
 
       @feed.failing_since.should eq date
-      RefreshFeedJob.perform @user.id, @feed.id
+      RefreshFeedJob.perform @refresh_feed_job_status.id
       @feed.reload.failing_since.should be_nil
     end
 
-    it 'does not change failing_since the second and sucesive times an update fails successively' do
+    it 'sets available to true when an update runs successfully' do
+      @feed.update available: false
+
+      @feed.reload.available.should be_false
+      RefreshFeedJob.perform @refresh_feed_job_status.id
+      @feed.reload.available.should be_true
+    end
+
+    it 'does not change failing_since if the feed update fails' do
       FeedClient.stub(:fetch).and_raise RestClient::Exception.new
       date = Time.zone.parse '2000-01-01'
       ActiveSupport::TimeZone.any_instance.stub(:now).and_return date
@@ -345,50 +148,17 @@ describe RefreshFeedJob do
       @feed.update failing_since: date2
 
       @feed.failing_since.should eq date2
-      RefreshFeedJob.perform @user.id, @feed.id
+      RefreshFeedJob.perform @refresh_feed_job_status.id
       @feed.reload.failing_since.should eq date2
     end
 
-    it 'marks feed as unavailable when it has been failing longer than a week' do
+    it 'does not mark feed as available when the feed update fails' do
       FeedClient.stub(:fetch).and_raise RestClient::Exception.new
-      date = Time.zone.parse '2000-01-01'
-      ActiveSupport::TimeZone.any_instance.stub(:now).and_return date
-      @feed.update failing_since: date - (1.week + 1.day)
+      @feed.update available:false
 
-      @feed.available.should be_true
-      RefreshFeedJob.perform @user.id, @feed.id
+      @feed.available.should be_false
+      RefreshFeedJob.perform @refresh_feed_job_status.id
       @feed.reload.available.should be_false
-    end
-
-    it 'unschedules updates for a feed when it has been failing longer than a week' do
-      FeedClient.stub(:fetch).and_raise RestClient::Exception.new
-      date = Time.zone.parse '2000-01-01'
-      ActiveSupport::TimeZone.any_instance.stub(:now).and_return date
-      @feed.update failing_since: date - (1.week + 1.day)
-
-      Resque.should_receive(:remove_schedule).with "update_feed_#{@feed.id}"
-
-      RefreshFeedJob.perform @user.id, @feed.id
-    end
-
-    it 'does not mark feed as unavailable when it has been failing a week but the next update is successful' do
-      FeedClient.stub :fetch
-      date = Time.zone.parse '2000-01-01'
-      ActiveSupport::TimeZone.any_instance.stub(:now).and_return date
-      @feed.update failing_since: date - (1.week + 1.day)
-
-      @feed.available.should be_true
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.available.should be_true
-    end
-
-    it 'does not mark feed as unavailable when it updates successfully' do
-      FeedClient.stub :fetch
-      @feed.update failing_since: nil
-
-      @feed.available.should be_true
-      RefreshFeedJob.perform @user.id, @feed.id
-      @feed.reload.available.should be_true
     end
   end
 
