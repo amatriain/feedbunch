18fa75faf0dd979ea7069254ee5f5caf0aef9e6f
Alfredo Amatriain <geralt@gmail.com>
2014-04-30 12:35:29 +0200

    Fixed tests.

--- spec/controllers/opml_imports_controller_spec.rb
+++ spec/controllers/opml_imports_controller_spec.rb
@@ -25,19 +25,19 @@ describe Api::OpmlImportsController do
 
     it 'redirects to main application page if successful' do
       User.any_instance.stub :import_subscriptions
-      post :create, opml_import_job_state: {file: 'mock_file'}
+      post :create, opml_import: {file: 'mock_file'}
       response.should redirect_to read_path
     end
 
     it 'redirects to main application page if an error happens' do
       User.any_instance.stub(:import_subscriptions).and_raise StandardError.new
-      post :create, opml_import_job_state: {file: 'mock_file'}
+      post :create, opml_import: {file: 'mock_file'}
       response.should redirect_to read_path
     end
 
     it 'creates a OpmlImportJobState instance with ERROR state if an error happens' do
       User.any_instance.stub(:import_subscriptions).and_raise StandardError.new
-      post :create, opml_import_job_state: {file: 'mock_file'}
+      post :create, opml_import: {file: 'mock_file'}
       @user.reload.opml_import_job_state.state.should eq OpmlImportJobState::ERROR
     end
   end
@@ -45,18 +45,18 @@ describe Api::OpmlImportsController do
   context 'PUT update' do
 
     it 'asigns the correct OpmlImportJobState' do
-      put :update, opml_import_job_state: {id: @user.opml_import_job_state.id, show_alert: 'false'}, format: :json
+      put :update, opml_import: {show_alert: 'false'}, format: :json
       assigns(:opml_import_job_state).should eq @user.opml_import_job_state
     end
 
     it 'returns success' do
-      put :update, opml_import: {id: @user.opml_import_job_state.id, show_alert: 'false'}, format: :json
+      put :update, opml_import: {show_alert: 'false'}, format: :json
       response.should be_success
     end
 
     it 'returns 500 if there is a problem changing the alert visibility' do
       User.any_instance.stub(:set_opml_import_job_state_visible).and_raise StandardError.new
-      put :update, opml_import_job_state: {id: @user.opml_import_job_state.id, show_alert: 'false'}, format: :json
+      put :update, opml_import: {show_alert: 'false'}, format: :json
       response.status.should eq 500
     end
   end
