9705e445acba3f37661de04efd4505a7760ce923
Alfredo Amatriain <geralt@gmail.com>
2014-05-13 09:43:02 +0200

    Upgraded ruby version from 2.1.1 to 2.1.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :application, 'feedbunch'
 #############################################################
 
 set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.1.1'
+set :rvm_ruby_version, 'ruby-2.1.2'
 set :rvm_user_path, '~/.rvm'
 
 #############################################################
