baaf7cc9895d04b462bb6f6814dbfe922cbefe0b
Alfredo Amatriain <geralt@gmail.com>
2014-06-02 17:27:34 +0200

    Fixed tests.

--- spec/jobs/fix_schedules_job_spec.rb
+++ spec/jobs/fix_schedules_job_spec.rb
@@ -5,7 +5,7 @@ describe FixSchedulesJob do
   before :each do
     @feed = FactoryGirl.create :feed
 
-    Resque.stub :get_schedule do
+    Resque.stub :fetch_schedule do
       {"class"=>"ScheduledUpdateFeedJob", "args"=>@feed.id, "every"=>"3600s"}
     end
   end
@@ -13,7 +13,7 @@ describe FixSchedulesJob do
   it 'adds missing scheduled feed updates' do
     feed_unscheduled = FactoryGirl.create :feed
     # @feed has scheduled updates, feed_unscheduled does not
-    Resque.stub :get_schedule do |name|
+    Resque.stub :fetch_schedule do |name|
       if name == "update_feed_#{@feed.id}"
         {"class"=>"ScheduledUpdateFeedJob", "args"=>@feed.id, "every"=>"3600s"}
       else
@@ -38,7 +38,7 @@ describe FixSchedulesJob do
 
     time_now = Time.zone.parse('2000-01-01 10:00:00')
     ActiveSupport::TimeZone.any_instance.stub(:now).and_return time_now
-    Resque.stub :get_schedule
+    Resque.stub :fetch_schedule
 
     # A job to schedule updates for @feed should be enqueued to be run at 2000-01-01 13:00:00
     Resque.should_receive(:set_schedule).once do |name, config|
@@ -58,7 +58,7 @@ describe FixSchedulesJob do
 
     time_now = Time.zone.parse('2000-01-02 01:00:00')
     ActiveSupport::TimeZone.any_instance.stub(:now).and_return time_now
-    Resque.stub :get_schedule
+    Resque.stub :fetch_schedule
 
     # A job to schedule updates for @feed should be enqueued to be run immediately
     Resque.should_receive(:set_schedule).once do |name, config|
@@ -73,7 +73,7 @@ describe FixSchedulesJob do
   end
 
   it 'schedules next update in the following hour if feed has never been updated' do
-    Resque.stub :get_schedule
+    Resque.stub :fetch_schedule
 
     # A job to schedule updates for @feed should be scheduled sometime during the next hour
     Resque.should_receive(:set_schedule).once do |name, config|
@@ -91,7 +91,7 @@ describe FixSchedulesJob do
   it 'does nothing for existing feed updates' do
     feed_scheduled = FactoryGirl.create :feed
     # @feed and feed_scheduled have scheduled updates
-    Resque.stub :get_schedule do |name|
+    Resque.stub :fetch_schedule do |name|
       if name == "update_feed_#{@feed.id}"
         {"class"=>"ScheduledUpdateFeedJob", "args"=>@feed.id, "every"=>"1h"}
       elsif name == "update_feed_#{feed_scheduled.id}"
@@ -107,7 +107,7 @@ describe FixSchedulesJob do
 
   it 'does not add a schedule for an unavailable feed' do
     @feed.update available: false
-    Resque.stub :get_schedule
+    Resque.stub :fetch_schedule
 
     Resque.should_not_receive :set_schedule
 
