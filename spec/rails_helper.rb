ca2381931a8d3965c6fca73e7107ec3a4a378d50
Alfredo Amatriain <geralt@gmail.com>
2014-06-06 13:31:08 +0200

    Auto-generated spec_helper.rb file and incoporated suggested changes in rails_helper.rb
    This brings rspec config in line with rspec and rspec-rails 3 best practices. See:
    
    https://github.com/rspec/rspec-rails#upgrading-an-existing-app

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ Coveralls.wear!('rails')
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
@@ -16,32 +17,22 @@ require 'nokogiri'
 # Factories instead of database fixtures
 require 'factory_girl_rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-# Runs pending migrations before tests are run.
+# Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!  if defined?(ActiveRecord::Migration)
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Yield the receiving instance as the first argument to any_instance implementation blocks.
-  # Optional but disabled by default in Rspec 2.99.0, enabled by default in Rspec 3
-  config.mock_with :rspec do |mocks|
-    mocks.yield_receiver_to_any_instance_implementation_blocks = true
-  end
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -67,17 +58,6 @@ RSpec.configure do |config|
       end
     end
   }
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = 'random'
 
   # To test controllers protected by Devise authentication
   config.include Devise::TestHelpers, :type => :controller
@@ -105,4 +85,19 @@ RSpec.configure do |config|
   # Include Warden helpers: login_as, logout...
   # For more about these helpers see Warden wiki: https://github.com/hassox/warden/wiki/Testing
   config.include Warden::Test::Helpers
+
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, :type => :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  #config.infer_spec_type_from_file_location!
 end
