cf640ca3500a7f4e2a2c5d94f337a0de54838344
Alfredo Amatriain <geralt@gmail.com>
2014-06-10 20:06:45 +0200

    Added acceptance test for the twitter share button.

---
+++ spec/acceptance_tests/suite_1/social_sharing_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'social sharing', type: :feature do
+
+  before :each do
+    @user = FactoryGirl.create :user
+    @feed = FactoryGirl.create :feed
+    @entry = FactoryGirl.build :entry, feed_id: @feed.id
+    @feed.entries << @entry
+    @user.subscribe @feed.fetch_url
+
+    login_user_for_feature @user
+    read_feed @feed, @user
+  end
+
+  it 'shows twitter share link', js: true do
+    read_entry @entry
+
+    within "#entry-#{@entry.id}-summary .entry-toolbar" do
+      expect(page).to have_css "a[target='_blank'][href='https://twitter.com/intent/tweet?url=#{@entry.url}&via=feedbunch&text=#{@entry.title}']"
+    end
+  end
+
+end
