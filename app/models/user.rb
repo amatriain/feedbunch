923392408d77dbe7f7a0ec2b935c21fd5f6dbc93
Alfredo Amatriain <geralt@gmail.com>
2014-06-12 15:54:34 +0200

    WIP: make the application invitation-only, by integrating devise_invitable gem.
    This will stay at least during the beta stage. Afterwards it would be nice if people could invite other people, but the invitation-only restriction will be lifted.

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525191641) do
+ActiveRecord::Schema.define(version: 20140612134049) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -164,7 +164,7 @@ ActiveRecord::Schema.define(version: 20140525191641) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "encrypted_password",     default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -188,10 +188,21 @@ ActiveRecord::Schema.define(version: 20140525191641) do
     t.boolean  "quick_reading",          default: false, null: false
     t.boolean  "open_all_entries",       default: false, null: false
     t.text     "name",                                   null: false
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.integer  "invitations_count",      default: 0
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count"
+  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
