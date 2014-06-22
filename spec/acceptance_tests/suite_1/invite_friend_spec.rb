656ca8cf07b24d5ef1aa828bcbc09cdb085ffe95
Alfredo Amatriain <geralt@gmail.com>
2014-06-23 00:54:38 +0200

    WIP: Acceptance tests for send invitation feature.
    Currently failing: user should be able to sign up normally instead of accepting an invitation. Currently if an invitation has been sent to a user's email, he must accept the invitation; he's unable to sign up normally, a "email has already been taken" error appears.

--- spec/acceptance_tests/suite_1/invite_friend_spec.rb
+++ spec/acceptance_tests/suite_1/invite_friend_spec.rb
@@ -97,6 +97,40 @@ describe 'invite friend', type: :feature do
     end
 =end
 
+    it 'displays an alert if there is an error sending an invitation', js: true do
+      allow(User).to receive(:exists?).and_raise StandardError.new
+      send_invitation @friend_email
+      should_show_alert 'problem-sending-invitation'
+    end
+
+    it 'can sign up instead of accepting an invitation', js: true do
+      # Friend is sent an invitation
+      send_invitation @friend_email
+      should_show_alert 'success-invite-friend'
+      mail_should_be_sent path: '/accept_invitation', to: @friend_email, text: 'Someone has invited you'
+      logout_user
+
+      # Friend signs up through the sign up view instead of clicking on the "accept invitation" link in the email
+      visit new_user_registration_path
+      friend_password = 'friend_password'
+      fill_in 'Email', with: @friend_email
+      fill_in 'Password', with: friend_password
+      fill_in 'Confirm password', with: friend_password
+      click_on 'Sign up'
+
+      # test that a confirmation email is sent
+      confirmation_link = mail_should_be_sent path: confirmation_path, to: @friend_email
+
+      # Test that user cannot login before confirming the email address
+      failed_login_user_for_feature @friend_email, friend_password
+
+      # Follow confirmation link received by email, user should be able to log in afterwards
+      visit confirmation_link
+      friend = User.find_by_email @friend_email
+      login_user_for_feature friend
+      user_should_be_logged_in
+    end
+
   end
 
   context 'accept invitation' do
@@ -104,12 +138,89 @@ describe 'invite friend', type: :feature do
     before :each do
       send_invitation @friend_email
       logout_user
-      @accept_link = mail_should_be_sent path: '/accept_invitation',
+      accept_link = mail_should_be_sent path: '/accept_invitation',
                                          to: @friend_email, text: 'Someone has invited you'
+
+      # Build the correct URL that must be used in testing for accepting invitations
+      uri = URI accept_link
+      accept_uri = URI accept_user_invitation_path
+      accept_uri.query = uri.query
+      @accept_url = accept_uri.to_s
+
+      @password = 'invited_password'
+    end
+
+    it 'cannot accept invitation with wrong token', js: true do
+      url = URI accept_user_invitation_path
+      url.query = 'invitation_token=wrong_token'
+      visit url
+      expect(page).to have_text 'The invitation token provided is not valid'
+    end
+
+    it 'cannot sign in before accepting invitation', js: true do
+      visit new_user_session_path
+      fill_in 'Email', with: @friend_email
+      fill_in 'Password', with: @password
+      click_on 'Sign in'
+
+      user_should_not_be_logged_in
     end
 
     it 'accepts invitation', js: true do
-      visit @accept_link
+      visit @accept_url
+      fill_in 'Password', with: @password
+      fill_in 'Confirm password', with: @password
+      click_on 'Activate account'
+
+      # User should be immediately logged in
+      expect(current_path).to eq read_path
+      user_should_be_logged_in
+
+      # User should be able to log in with the chosen password
+      logout_user
+      visit new_user_session_path
+      fill_in 'Email', with: @friend_email
+      fill_in 'Password', with: @password
+      click_on 'Sign in'
+
+      user_should_be_logged_in
+    end
+
+    it 'accepts invitation from resent invitation email', js: true do
+      login_user_for_feature @user
+      # Send second invitation
+      send_invitation @friend_email
+      should_show_alert 'success-invitation-resend'
+      logout_user
+
+      # Link in second invitation should be the same as in the first
+      resent_link = mail_should_be_sent path: '/accept_invitation',
+                                        to: @friend_email, text: 'Someone has invited you'
+      resent_uri = URI resent_link
+      resent_accept_uri = URI accept_user_invitation_path
+      resent_accept_uri.query = resent_uri.query
+      resent_accept_url = resent_accept_uri.to_s
+
+      expect(resent_accept_url).to eq @accept_url
+
+      # Accept invitation from second email
+      visit resent_accept_url
+      fill_in 'Password', with: @password
+      fill_in 'Confirm password', with: @password
+      click_on 'Activate account'
+
+      # User should be immediately logged in
+      expect(current_path).to eq read_path
+      user_should_be_logged_in
+
+      # User should be able to log in with the chosen password
+      logout_user
+      visit new_user_session_path
+      fill_in 'Email', with: @friend_email
+      fill_in 'Password', with: @password
+      click_on 'Sign in'
+
+      user_should_be_logged_in
     end
 
   end
