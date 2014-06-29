cf079eb6cbb7aa2a44c386edf4545f011275a46b
Alfredo Amatriain <geralt@gmail.com>
2014-06-29 14:02:47 +0200

    Moved duplicated code that returns a URL params array keeping the invitation_token if present to a helper method.

--- app/helpers/uri_helpers.rb
+++ app/helpers/uri_helpers.rb
@@ -32,4 +32,24 @@ module UriHelpers
     end
     return fixed_url
   end
+
+  ##
+  # Return a params array that can be used in url_for, path_for methods. The returned params will have the
+  # invitation_token if and only if this param is present in the params array sent with the current request.
+  #
+  # The params array for the current request must be passed.
+  # Optionally a locale can also be passed; if so, the locale param will also be in the params array returned.
+  #
+  # This helper is used in places in which a link href may have the invitation_token and locale parameters,
+  # or they me be absent from the URL instead of present but with a blank value. E.g.: locale switch links in
+  # devise views must have the invitation_token param to be valid, but only in the accept invitation view; in
+  # the rest of views the locale links must not have this param.
+
+  def self.params_keep_invitation_token(params, locale: nil)
+    invitation_token = params[:invitation_token]
+    params = {}.
+      merge( (locale.present?) ? {locale: locale} : {} ).
+      merge( (invitation_token.present?) ? {invitation_token: invitation_token} : {} )
+    return params
+  end
 end
