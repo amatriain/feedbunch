b8ba9474cee2a7e09efe8ceb129bf181dbf09ada
Alfredo Amatriain <geralt@gmail.com>
2014-09-16 13:21:04 +0200

    Implemented acceptance test.

--- spec/acceptance_tests/suite_2/application_tours_spec.rb
+++ spec/acceptance_tests/suite_2/application_tours_spec.rb
@@ -19,14 +19,23 @@ describe 'application tours', type: :feature do
         tour_should_be_visible 'Start'
       end
 
-      it 'does not show the tour after finishing it'
+      it 'does not show the tour after finishing it', js: true do
+        while page.has_css? '.hopscotch-next'
+          find('.hopscotch-next').click
+        end
+        tour_should_not_be_visible
+
+        visit read_path
+        # wait for client code to initialize
+        sleep 1
+        tour_should_not_be_visible
+      end
 
       it 'does not show the tour after forcibly closing it'
 
     end
 
     context 'returning users' do
-
 
       it 'does not show the tour'
 
