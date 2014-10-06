4385ff94ffcd5f447357be10c904c96d8e70c052
Alfredo Amatriain <geralt@gmail.com>
2014-10-06 16:10:59 +0200

    Converted UnsubscribeUserJob to a Sidekiq worker

---
+++ app/workers/unsubscribe_user_worker.rb
@@ -0,0 +1,42 @@
+##
+# Background job to unsubscribe a user from a feed
+#
+# This is a Sidekiq worker
+
+class UnsubscribeUserWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :subscriptions
+
+  ##
+  # Unsubscribe a user from a feed
+  #
+  # Receives as arguments:
+  # - id of the user
+  # - id of the feed
+  #
+  # This method is intended to be invoked from Sidekiq, which means it is performed in the background.
+
+  def perform(user_id, feed_id)
+    # Check if the user actually exists
+    if !User.exists? user_id
+      Rails.logger.error "Trying to unsubscribe non-existing user @#{user_id}, aborting job"
+      return
+    end
+    user = User.find user_id
+
+    # Check if the feed actually exists and is subscribed by the user
+    if !Feed.exists? feed_id
+      Rails.logger.error "Trying to unsubscribe user #{user.id} - #{user.email} from non-existing feed #{feed_id}, aborting job"
+      return
+    end
+    feed = Feed.find feed_id
+    if !user.feeds.include? feed
+      Rails.logger.error "Trying to unsubscribe user #{user.id} - #{user.email} from feed #{feed.id} - #{feed.title} to which he's not subscribed, aborting job"
+      return
+    end
+
+    Rails.logger.info "Unsubscribing user #{user.id} - #{user.email} from feed #{feed.id} - #{feed.title}"
+    user.unsubscribe feed
+  end
+end
---
+++ spec/unit_tests/workers/unsubscribe_user_worker_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe UnsubscribeUserWorker do
+
+  before :each do
+    @user = FactoryGirl.create :user
+    @feed = FactoryGirl.create :feed
+    @user.subscribe @feed.fetch_url
+  end
+
+  it 'unsubscribes user from feed' do
+    expect(@user.feeds).to include @feed
+    UnsubscribeUserWorker.new.perform @user.id, @feed.id
+    @user.reload
+    expect(@user.feeds).not_to include @feed
+  end
+
+  context 'validations' do
+
+    it 'does nothing if the user does not exist' do
+      expect_any_instance_of(User).not_to receive :unsubscribe
+      UnsubscribeUserWorker.new.perform 1234567890, @feed.id
+    end
+
+    it 'does nothing if the feed does not exist' do
+      expect(@user).not_to receive :unsubscribe
+      UnsubscribeUserWorker.new.perform @user.id, 1234567890
+    end
+
+    it 'does nothing if the feed is not subscribed by the user' do
+      folder = FactoryGirl.create :folder
+      expect(@user).not_to receive :subscribe
+      SubscribeUserJob.perform @user.id, @feed.fetch_url, folder.id, false, nil
+    end
+
+  end
+end
