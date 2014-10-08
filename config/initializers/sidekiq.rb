9991212f80aea2db2f22508c6d75d6e9d58c92a5
Alfredo Amatriain <geralt@gmail.com>
2014-10-08 15:42:34 +0200

    Sidetiq uses the rails log file.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,4 +7,8 @@ Sidekiq.configure_client do |config|
   config.redis = { url: Rails.application.secrets.redis_web }
 end
 
+# Show error backtraces
 Sidekiq.default_worker_options = { 'backtrace' => true }
+
+# Log Sidetiq messages to the rails log
+Sidetiq.logger = Rails.logger
