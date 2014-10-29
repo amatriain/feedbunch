07ba2dbf7cbff020bbce8ec957bfb36ef1d6fd9a
Alfredo Amatriain <geralt@gmail.com>
2014-10-29 23:17:23 +0100

    Minor changes in puma config.

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,5 +3,6 @@ environment 'production'
 threads 8,8 
 bind 'unix:///tmp/feedbunch_puma.sock'
 workers 2
+daemonize true
 pidfile '/tmp/feedbunch_puma.pid'
-stdout_redirect '/var/log/feedbunch_puma.log'
+stdout_redirect '/var/log/feedbunch-puma.log'
