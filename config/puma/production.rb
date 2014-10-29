80c16f274aa019d1d5460a44a6b266a6ce52fa16
Alfredo Amatriain <geralt@gmail.com>
2014-10-29 23:21:25 +0100

    Minor change in Puma config.

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,8 +1,8 @@
 directory '/var/rails/feedbunch/current'
 environment 'production'
 threads 8,8 
-bind 'unix:///tmp/feedbunch_puma.sock'
+bind 'unix:///tmp/feedbunch-puma.sock'
 workers 2
 daemonize true
-pidfile '/tmp/feedbunch_puma.pid'
+pidfile '/tmp/feedbunch-puma.pid'
 stdout_redirect '/var/log/feedbunch-puma.log'
