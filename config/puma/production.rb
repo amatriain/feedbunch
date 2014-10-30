939e36d35cdd9414826692a364d2f601894029fa
amatriain <geralt@gmail.com>
2014-10-30 19:16:02 +0100

    Changes in puma configuration:
    - 2 workers, 4 threads each. This means a total of 8 simultaneous connections, 8 database connections open from the web server. This is more or less what I can afford with the memory in my VPS.
    - do not daemonize the Puma process. It's much better if Upstart takes care of daemonizing it in production, tracking its correct PID and being in charge of respawning Puma if necessary.

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,8 +1,7 @@
 directory '/var/rails/feedbunch/current'
 environment 'production'
-threads 8,8 
+threads 4,4 
 bind 'unix:///tmp/feedbunch-puma.sock'
 workers 2
-daemonize true
 pidfile '/tmp/feedbunch-puma.pid'
 stdout_redirect '/var/log/feedbunch-puma.log'
