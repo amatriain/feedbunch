67527a9bd6897d8d7629cc4c5274bb35ebe79dd3
Alfredo Amatriain <geralt@gmail.com>
2014-11-09 19:57:58 +0100

    Created worker to update Redmon stats every minute.
    It is a Sidekiq worker, scheduled with Sidetiq to run every minute. It collects stats from the Redis instance used as Rails cache backend, which can then be visualized with Redmon at the /redmon path.

---
+++ app/workers/update_redis_cache_stats_worker.rb
@@ -0,0 +1,28 @@
+##
+# Background worker to update stats for the Redis instance serving as Rails cache backend.
+# Stats are then visualized with Redmon, accessible at the /redmon path.
+#
+# This is a Sidekiq worker
+
+class UpdateRedisCacheStatsWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  # This worker runs periodically. Do not retry.
+  sidekiq_options retry: false, queue: :maintenance
+  # Run every minute.
+  recurrence do
+    minutely
+  end
+
+  ##
+  # Update stats (memory use, configuration, last time saved etc) from the Redis instance serving as Rails cache backend.
+  # Stats can be visualized with Redmon, accessible at the /redmon path.
+
+  def perform
+    Rails.logger.debug 'Updating Redmon stats for the Redis rails cache instance'
+    worker = Redmon::Worker.new
+    worker.record_stats
+    worker.cleanup_old_stats
+  end
+end
