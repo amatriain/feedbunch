69e7a698d341c9737950727a539aa8a9ba05485d
Alfredo Amatriain <geralt@gmail.com>
2014-11-16 19:01:43 +0100

    WIP: update subscriptions_updated_at attribute in User instances at the appropiate times.

--- spec/unit_tests/models/user/user_subscriptions_updated_at_spec.rb
+++ spec/unit_tests/models/user/user_subscriptions_updated_at_spec.rb
@@ -37,24 +37,36 @@ describe User, type: :model do
         @feed.reload.update url: 'http://another.url.com'
         expect(@user.reload.subscriptions_updated_at).to be > @old_subscriptions_updated_at
       end
-    end
 
-    context 'unread entries count' do
+      context 'unread entries count' do
 
-      it 'when unread entries count for a feed is incremented' do
+        before :each do
+          # @entry1 = FactoryGirl.build :entry, feed_id: @feed.id
+          # @feed.entries << @entry1
+          # expect(@user.feed_unread_count @feed).to eq 1
+          # @old_subscriptions_updated_at = @user.reload.subscriptions_updated_at
+        end
 
+        it 'when unread entries count for a feed is incremented' do
+          pending 'work in progress'
+          entry2 = FactoryGirl.build :entry, feed_id: @feed.id
+          @feed.entries << entry2
+          expect(@user.feed_unread_count @feed).to eq 2
+          expect(@user.reload.subscriptions_updated_at).to be > @old_subscriptions_updated_at
+        end
+
+        it 'when unread entries for a feed is decremented'
+
+        it 'when unread entries count for a feed is set to zero'
       end
 
-      it 'when unread entries for a feed is decremented'
+      context 'folder changes' do
 
-      it 'when unread entries count for a feed is set to zero'
+        it 'when feed is moved into a folder'
+
+        it 'when feed is moved out of a folder'
+      end
     end
 
-    context 'folder changes' do
-
-      it 'when feed is moved into a folder'
-
-      it 'when feed is moved out of a folder'
-    end
   end
 end
