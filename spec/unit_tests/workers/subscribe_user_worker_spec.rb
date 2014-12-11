d6d699e3d0f0b85915083b74f8b703e4da8c16b8
Alfredo Amatriain <geralt@gmail.com>
2014-12-11 16:10:18 +0100

    WIP: adapt tests to recent changes in subscribe and OPML import workers.

--- spec/unit_tests/workers/subscribe_user_worker_spec.rb
+++ spec/unit_tests/workers/subscribe_user_worker_spec.rb
@@ -8,6 +8,8 @@ describe SubscribeUserWorker do
     @user.folders << @folder
     @feed = FactoryGirl.create :feed
     @url = 'http://www.galactanet.com/feed.xml'
+    @job_state = FactoryGirl.build :subscribe_job_state, user_id: @user.id, fetch_url: @feed.fetch_url
+    @user.subscribe_job_states << @job_state
 
     # Stub FeedClient.stub so that it does not actually fetch feeds, but returns them untouched
     allow(FeedClient).to receive :fetch do |feed, perform_autodiscovery|
@@ -17,14 +19,14 @@ describe SubscribeUserWorker do
 
   it 'subscribes user to already existing feeds' do
     expect(@user.feeds).not_to include @feed
-    SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @folder.id, false, nil
+    SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @job_state.id
     @user.reload
     expect(@user.feeds).to include @feed
   end
 
   it 'creates new feeds and subscribes user to them' do
     expect(Feed.exists?(fetch_url: @url)).to be false
-    SubscribeUserWorker.new.perform @user.id, @url, @folder.id, false, nil
+    SubscribeUserWorker.new.perform @user.id, @url, @job_state.id
     @user.reload
     expect(@user.feeds.where(fetch_url: @url)).to be_present
   end
@@ -35,42 +37,36 @@ describe SubscribeUserWorker do
       expect(autodiscovery).to be true
       feed
     end
-    SubscribeUserWorker.new.perform @user.id, @url, @folder.id, false, nil
+    SubscribeUserWorker.new.perform @user.id, @url, @job_state.id
   end
 
   context 'validations' do
 
     it 'does nothing if the user does not exist' do
       expect_any_instance_of(User).not_to receive :subscribe
-      SubscribeUserWorker.new.perform 1234567890, @feed.fetch_url, @folder.id, false, nil
+      SubscribeUserWorker.new.perform 1234567890, @feed.fetch_url, @job_state.id
     end
 
     it 'destroys subscribe_job_state if the user does not exist' do
       subscribe_job_state = FactoryGirl.create :subscribe_job_state, user_id: @user.id, fetch_url: @feed.fetch_url
       @user.delete
 
+      expect(SubscribeJobState.count).to eq 2
+      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, subscribe_job_state.id
       expect(SubscribeJobState.count).to eq 1
-      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @folder.id, false, subscribe_job_state.id
-      expect(SubscribeJobState.count).to eq 0
     end
 
-    it 'does nothing if the folder does not exist' do
-      expect(@user).not_to receive :subscribe
-      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, 1234567890, false, nil
+    it 'does nothing if job_state does not exist' do
+      expect_any_instance_of(User).not_to receive :subscribe
+      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, 'not_a_valid_id'
     end
 
-    it 'does nothing if the folder is not owned by the user' do
-      folder = FactoryGirl.create :folder
-      expect(@user).not_to receive :subscribe
-      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, folder.id, false, nil
-    end
-
-    it 'does nothing if the job_status is not in state RUNNING' do
+    it 'does nothing if the job_state is not in state RUNNING' do
       job_state = FactoryGirl.build :subscribe_job_state, user_id: @user.id, fetch_url: @feed.fetch_url,
                                     state: SubscribeJobState::ERROR
       @user.subscribe_job_states << job_state
       expect(@user).not_to receive :subscribe
-      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @folder.id, false, job_state.id
+      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, job_state.id
     end
 
   end
@@ -120,27 +116,22 @@ describe SubscribeUserWorker do
 
   context 'updates job state' do
 
-    before :each do
-      @job_state = FactoryGirl.build :subscribe_job_state, user_id: @user.id, fetch_url: @feed.fetch_url
-      @user.subscribe_job_states << @job_state
-    end
-
     it 'sets state to SUCCESS if job finishes successfully' do
       expect(@job_state.state).to eq SubscribeJobState::RUNNING
-      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @folder.id, false, @job_state.id
+      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @job_state.id
       expect(@job_state.reload.state).to eq SubscribeJobState::SUCCESS
     end
 
     it 'saves feed id if job finishes successfully' do
       expect(@job_state.feed).to be_blank
-      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @folder.id, false, @job_state.id
+      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @job_state.id
       expect(@job_state.reload.feed).to eq @feed
     end
 
     it 'sets state to ERROR if job finishes with an error' do
       allow_any_instance_of(User).to receive(:subscribe).and_raise SocketError.new
       expect(@job_state.state).to eq SubscribeJobState::RUNNING
-      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @folder.id, false, @job_state.id
+      SubscribeUserWorker.new.perform @user.id, @feed.fetch_url, @job_state.id
       expect(@job_state.reload.state).to eq SubscribeJobState::ERROR
     end
 
