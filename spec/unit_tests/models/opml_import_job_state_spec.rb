dee3cd6f2bc3c75ca0f0d3d8aaca6e9660800b5e
amatriain <geralt@gmail.com>
2014-12-20 13:58:13 +0100

    Added unit tests for the relationship between OpmlImportJobState and OpmlImportFailure instances.

--- spec/unit_tests/models/opml_import_job_state_spec.rb
+++ spec/unit_tests/models/opml_import_job_state_spec.rb
@@ -18,7 +18,6 @@ describe OpmlImportJobState, type: :model do
 
     it 'defaults to state NONE when created' do
       @user.create_opml_import_job_state
-
       expect(@user.opml_import_job_state.state).to eq OpmlImportJobState::NONE
     end
 
@@ -38,6 +37,34 @@ describe OpmlImportJobState, type: :model do
       expect(opml_import_job_state.show_alert).to be true
     end
 
+  end
+
+  context 'relationship with OpmlImportFailures' do
+
+    before :each do
+      @job_state = FactoryGirl.create :opml_import_job_state
+    end
+
+    it 'destroys associated OpmlImportFailure instances when destroyed' do
+      import_failure_1 = FactoryGirl.build :opml_import_failure, opml_import_job_state_id: @job_state.id
+      import_failure_2 = FactoryGirl.build :opml_import_failure, opml_import_job_state_id: @job_state.id
+      @job_state.opml_import_failures << import_failure_1 << import_failure_2
+
+      expect(OpmlImportFailure.all.count).to eq 2
+      @job_state.destroy
+      expect(OpmlImportFailure.all.count).to eq 0
+    end
+
+    it 'does not destroy OpmlImportFailure instances associated with other jobs' do
+      job_state_2 = FactoryGirl.create :opml_import_job_state
+      import_failure_1 = FactoryGirl.build :opml_import_failure, opml_import_job_state_id: job_state_2.id
+      import_failure_2 = FactoryGirl.build :opml_import_failure, opml_import_job_state_id: job_state_2.id
+      job_state_2.opml_import_failures << import_failure_1 << import_failure_2
+
+      expect(OpmlImportFailure.all.count).to eq 2
+      @job_state.destroy
+      expect(OpmlImportFailure.all.count).to eq 2
+    end
   end
 
 end
