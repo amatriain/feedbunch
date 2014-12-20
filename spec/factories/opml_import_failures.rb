a14e16e664b1b6fd7f5d9c3104ac87200c28bbd0
amatriain <geralt@gmail.com>
2014-12-20 13:52:08 +0100

    Fixed factory definition name.

--- spec/factories/opml_import_failures.rb
+++ spec/factories/opml_import_failures.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   sequence(:opml_import_failure_url_sequence) {|n| "http://some.failed.feed_#{n}.com"}
 
-  factory :opml_import_faillure do
+  factory :opml_import_failure do
     url {generate :opml_import_failure_url_sequence}
     opml_import_job_state
   end
