da8fbb957c393c6c319e69ada7ada43f510ed38e
Alfredo Amatriain <geralt@gmail.com>
2015-01-06 00:09:32 +0100

    Added whitelists for tags and attributes when sanitizing.
    They are used by the new loofah-based sanitizer in Rails 4.2. We use this sanitizer when sanitizing external outputs (entries, folder titles etc).

--- config/application.rb
+++ config/application.rb
@@ -48,11 +48,21 @@ module Feedbunch
       g.fixture_replacement :factory_girl
     end
 
-    # Add "target" html attribute to the whitelist when using the sanitize helpers.
-    # This is necessary because otherwise all "target" attributes are filtered out, which means that the app cannot
-    # create links with target="_blank" that open in a new tab.
-    # We really want links in feed entries to open in a new tab!
-    config.action_view.sanitized_allowed_attributes = %w(target)
+    # Whitelist of accepted tags and attributes when sanitizing entries, folder titles etc.
+    # This list is a more restrictive version of:
+    # https://github.com/flavorjones/loofah/blob/master/lib/loofah/html5/whitelist.rb
+    config.action_view.sanitized_allowed_attributes = %w[alt border cite colspan color coords datetime
+      dir headers href hreflang ismap label lang loop loopcount loopend loopstart media poster preload
+      rel rev rowspan scope shape span src start summary target title usemap]
+    config.action_view.sanitized_allowed_tags = %w[a abbr acronym address area
+      article aside audio b bdi bdo big blockquote br canvas
+      caption center cite code col colgroup dd del
+      dfn div dl dt em figcaption figure footer
+      h1 h2 h3 h4 h5 h6 header hr i img ins kbd
+      li map mark nav ol p
+      pre q s samp section small span strike strong sub
+      sup table tbody td tfoot th thead time tr tt u ul var
+      video]
 
     # Most devise views use the devise layout except "edit_registration", which uses its own layout
     config.to_prepare do
