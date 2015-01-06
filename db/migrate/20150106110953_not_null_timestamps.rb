dac799d666589ce91b69375bfde6200c264dada4
Alfredo Amatriain <geralt@gmail.com>
2015-01-06 13:54:29 +0100

    Added missing timestamps columns to all tables where necessary. Added NOT NULL constraint to all timestamp columns.
    This should help transition to Rails 5 in the future, when timestamp columns will be not-null by default.

---
+++ db/migrate/20150106110953_not_null_timestamps.rb
@@ -0,0 +1,48 @@
+class NotNullTimestamps < ActiveRecord::Migration
+  def change
+    change_column_null :active_admin_comments, :created_at, false, Time.zone.now
+    change_column_null :active_admin_comments, :updated_at, false
+
+    add_timestamps :deleted_entries, null: true
+    change_column_null :deleted_entries, :created_at, false, Time.zone.now
+    change_column_null :deleted_entries, :updated_at, false, Time.zone.now
+
+    change_column_null :entries, :created_at, false, Time.zone.now
+    change_column_null :entries, :updated_at, false, Time.zone.now
+
+    change_column_null :entry_states, :created_at, false, Time.zone.now
+    change_column_null :entry_states, :updated_at, false, Time.zone.now
+
+    change_column_null :feed_subscriptions, :created_at, false, Time.zone.now
+    change_column_null :feed_subscriptions, :updated_at, false, Time.zone.now
+
+    change_column_null :feeds, :created_at, false, Time.zone.now
+    change_column_null :feeds, :updated_at, false, Time.zone.now
+
+    add_timestamps :feeds_folders, null: true
+    change_column_null :feeds_folders, :created_at, false, Time.zone.now
+    change_column_null :feeds_folders, :updated_at, false, Time.zone.now
+
+    change_column_null :folders, :created_at, false, Time.zone.now
+    change_column_null :folders, :updated_at, false, Time.zone.now
+
+    change_column_null :opml_export_job_states, :created_at, false, Time.zone.now
+    change_column_null :opml_export_job_states, :updated_at, false, Time.zone.now
+
+    add_timestamps :opml_import_failures, null: true
+    change_column_null :opml_import_failures, :created_at, false, Time.zone.now
+    change_column_null :opml_import_failures, :updated_at, false, Time.zone.now
+
+    change_column_null :opml_import_job_states, :created_at, false, Time.zone.now
+    change_column_null :opml_import_job_states, :updated_at, false, Time.zone.now
+
+    change_column_null :refresh_feed_job_states, :created_at, false, Time.zone.now
+    change_column_null :refresh_feed_job_states, :updated_at, false, Time.zone.now
+
+    change_column_null :subscribe_job_states, :created_at, false, Time.zone.now
+    change_column_null :subscribe_job_states, :updated_at, false, Time.zone.now
+
+    change_column_null :users, :created_at, false, Time.zone.now
+    change_column_null :users, :updated_at, false, Time.zone.now
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,31 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141220103712) do
+ActiveRecord::Schema.define(version: 20150106110953) do
 
-  create_table "active_admin_comments", force: true do |t|
-    t.string   "namespace"
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace",     limit: 255
     t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
+    t.string   "resource_id",   limit: 255, null: false
+    t.string   "resource_type", limit: 255, null: false
     t.integer  "author_id"
-    t.string   "author_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "author_type",   limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
-  create_table "deleted_entries", force: true do |t|
-    t.integer "feed_id", null: false
-    t.text    "guid",    null: false
+  create_table "deleted_entries", force: :cascade do |t|
+    t.integer  "feed_id",    null: false
+    t.text     "guid",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "deleted_entries", ["guid", "feed_id"], name: "index_deleted_entries_on_guid_feed_id"
 
-  create_table "entries", force: true do |t|
+  create_table "entries", force: :cascade do |t|
     t.text     "title",                       null: false
     t.text     "url",                         null: false
     t.text     "author"
@@ -44,20 +46,20 @@ ActiveRecord::Schema.define(version: 20141220103712) do
     t.datetime "published",                   null: false
     t.text     "guid",                        null: false
     t.integer  "feed_id",                     null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   add_index "entries", ["feed_id"], name: "index_entries_on_feed_id"
   add_index "entries", ["guid", "feed_id"], name: "index_entries_on_guid_feed_id"
   add_index "entries", ["published", "created_at", "id"], name: "index_entries_on_published_created_at_id"
 
-  create_table "entry_states", force: true do |t|
+  create_table "entry_states", force: :cascade do |t|
     t.boolean  "read",       default: false, null: false
     t.integer  "user_id",                    null: false
     t.integer  "entry_id",                   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "entry_states", ["entry_id", "user_id"], name: "index_entry_states_on_entry_id_user_id"
@@ -65,12 +67,12 @@ ActiveRecord::Schema.define(version: 20141220103712) do
   add_index "entry_states", ["read", "user_id"], name: "index_entry_states_on_read_user_id"
   add_index "entry_states", ["user_id"], name: "index_entry_states_on_user_id"
 
-  create_table "feed_subscriptions", force: true do |t|
+  create_table "feed_subscriptions", force: :cascade do |t|
     t.integer  "user_id",                    null: false
     t.integer  "feed_id",                    null: false
     t.integer  "unread_entries", default: 0, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "feed_subscriptions", ["feed_id", "user_id"], name: "index_feed_subscriptions_on_feed_id_user_id"
@@ -78,11 +80,11 @@ ActiveRecord::Schema.define(version: 20141220103712) do
   add_index "feed_subscriptions", ["user_id", "unread_entries"], name: "index_feed_subscriptions_on_user_id_unread_entries"
   add_index "feed_subscriptions", ["user_id"], name: "index_feed_subscriptions_on_user_id"
 
-  create_table "feeds", force: true do |t|
+  create_table "feeds", force: :cascade do |t|
     t.text     "title",                              null: false
     t.text     "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.text     "fetch_url",                          null: false
     t.datetime "last_fetched"
     t.integer  "fetch_interval_secs", default: 3600, null: false
@@ -95,119 +97,123 @@ ActiveRecord::Schema.define(version: 20141220103712) do
   add_index "feeds", ["title"], name: "index_feeds_on_title"
   add_index "feeds", ["url"], name: "index_feeds_on_url"
 
-  create_table "feeds_folders", force: true do |t|
-    t.integer "feed_id",   null: false
-    t.integer "folder_id", null: false
+  create_table "feeds_folders", force: :cascade do |t|
+    t.integer  "feed_id",    null: false
+    t.integer  "folder_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "feeds_folders", ["feed_id"], name: "index_feeds_folders_on_feed_id"
   add_index "feeds_folders", ["folder_id"], name: "index_feeds_folders_on_folder_id"
 
-  create_table "folders", force: true do |t|
+  create_table "folders", force: :cascade do |t|
     t.integer  "user_id",                  null: false
     t.text     "title",                    null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.datetime "subscriptions_updated_at"
   end
 
   add_index "folders", ["user_id", "title"], name: "index_folders_on_user_id_title"
   add_index "folders", ["user_id"], name: "index_folders_on_user_id"
 
-  create_table "opml_export_job_states", force: true do |t|
+  create_table "opml_export_job_states", force: :cascade do |t|
     t.integer  "user_id",                    null: false
     t.text     "state",                      null: false
     t.boolean  "show_alert",  default: true, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.text     "filename"
     t.datetime "export_date"
   end
 
   add_index "opml_export_job_states", ["user_id"], name: "index_opml_export_job_states_on_user_id"
 
-  create_table "opml_import_failures", force: true do |t|
-    t.integer "opml_import_job_state_id", null: false
-    t.text    "url",                      null: false
+  create_table "opml_import_failures", force: :cascade do |t|
+    t.integer  "opml_import_job_state_id", null: false
+    t.text     "url",                      null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   add_index "opml_import_failures", ["opml_import_job_state_id"], name: "index_opml_import_failures_on_job_state_id"
 
-  create_table "opml_import_job_states", force: true do |t|
+  create_table "opml_import_job_states", force: :cascade do |t|
     t.integer  "user_id",                        null: false
     t.text     "state",                          null: false
     t.integer  "total_feeds",     default: 0,    null: false
     t.integer  "processed_feeds", default: 0,    null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.boolean  "show_alert",      default: true, null: false
   end
 
   add_index "opml_import_job_states", ["user_id"], name: "index_opml_import_job_states_on_user_id"
 
-  create_table "refresh_feed_job_states", force: true do |t|
+  create_table "refresh_feed_job_states", force: :cascade do |t|
     t.integer  "user_id",    null: false
     t.integer  "feed_id",    null: false
     t.text     "state",      null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "refresh_feed_job_states", ["created_at"], name: "index_refresh_feed_job_states_on_created_at"
   add_index "refresh_feed_job_states", ["user_id"], name: "index_refresh_feed_job_states_on_user_id"
 
-  create_table "subscribe_job_states", force: true do |t|
+  create_table "subscribe_job_states", force: :cascade do |t|
     t.integer  "user_id",    null: false
     t.text     "state",      null: false
     t.text     "fetch_url",  null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "feed_id"
   end
 
   add_index "subscribe_job_states", ["created_at"], name: "index_subscribe_job_states_on_created_at"
   add_index "subscribe_job_states", ["user_id"], name: "index_subscribe_job_states_on_user_id"
 
-  create_table "users", force: true do |t|
-    t.string   "email",                        default: "",    null: false
-    t.string   "encrypted_password",           default: ""
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                        limit: 255, default: "",    null: false
+    t.string   "encrypted_password",           limit: 255, default: ""
+    t.string   "reset_password_token",         limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                default: 0
+    t.integer  "sign_in_count",                            default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string   "current_sign_in_ip",           limit: 255
+    t.string   "last_sign_in_ip",              limit: 255
+    t.string   "confirmation_token",           limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",              default: 0
-    t.string   "unlock_token"
+    t.string   "unconfirmed_email",            limit: 255
+    t.integer  "failed_attempts",                          default: 0
+    t.string   "unlock_token",                 limit: 255
     t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "admin",                        default: false, null: false
-    t.text     "locale",                                       null: false
-    t.text     "timezone",                                     null: false
-    t.boolean  "quick_reading",                default: false, null: false
-    t.boolean  "open_all_entries",             default: false, null: false
-    t.text     "name",                                         null: false
-    t.string   "invitation_token"
+    t.datetime "created_at",                                               null: false
+    t.datetime "updated_at",                                               null: false
+    t.boolean  "admin",                                    default: false, null: false
+    t.text     "locale",                                                   null: false
+    t.text     "timezone",                                                 null: false
+    t.boolean  "quick_reading",                            default: false, null: false
+    t.boolean  "open_all_entries",                         default: false, null: false
+    t.text     "name",                                                     null: false
+    t.string   "invitation_token",             limit: 255
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
-    t.string   "invited_by_type"
-    t.integer  "invitations_count",            default: 0
-    t.string   "unencrypted_invitation_token"
+    t.string   "invited_by_type",              limit: 255
+    t.integer  "invitations_count",                        default: 0
+    t.string   "unencrypted_invitation_token", limit: 255
     t.datetime "invitations_count_reset_at"
-    t.boolean  "show_main_tour",               default: true,  null: false
-    t.boolean  "show_mobile_tour",             default: true,  null: false
-    t.boolean  "show_feed_tour",               default: true,  null: false
-    t.boolean  "show_entry_tour",              default: true,  null: false
+    t.boolean  "show_main_tour",                           default: true,  null: false
+    t.boolean  "show_mobile_tour",                         default: true,  null: false
+    t.boolean  "show_feed_tour",                           default: true,  null: false
+    t.boolean  "show_entry_tour",                          default: true,  null: false
     t.datetime "subscriptions_updated_at"
     t.datetime "folders_updated_at"
     t.datetime "refresh_feed_jobs_updated_at"
