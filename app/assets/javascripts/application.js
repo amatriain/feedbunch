6eb73bc90b01df555d461ea8236b0ff151e7f958
amatriain <geralt@gmail.com>
2015-02-24 13:13:36 +0100

    Updated fastclick.js 1.0.3 -> 1.0.6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 // require ../../../vendor/assets/javascripts/bootlint-0.11.0
 
 //= require ../../../vendor/assets/javascripts/enquire-2.1.2.min
-//= require ../../../vendor/assets/javascripts/fastclick-1.0.3.min
+//= require ../../../vendor/assets/javascripts/fastclick-1.0.6.min
 //= require ../../../vendor/assets/javascripts/favico-0.3.5.min
 //= require ../../../vendor/assets/javascripts/hopscotch-0.2.2.min
 //= require ../../../vendor/assets/javascripts/jquery.withinViewport-0.2
