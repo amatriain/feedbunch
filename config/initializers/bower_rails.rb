60a230c1e30e3590fb55c8e13d49c15c82f6260c
Alfredo Amatriain <geralt@gmail.com>
2015-03-20 12:52:33 +0100

    Force bower to resolve relative paths in components before precompiling assets with the rails asset pipeline.
    Necessary for relative paths not to be broken when deploying with capistrano.

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -6,7 +6,7 @@ BowerRails.configure do |bower_rails|
   # bower_rails.install_before_precompile = true
 
   # Invokes rake bower:resolve before precompilation. Defaults to false
-  # bower_rails.resolve_before_precompile = true
+  bower_rails.resolve_before_precompile = true
 
   # Invokes rake bower:clean before precompilation. Defaults to false
   # bower_rails.clean_before_precompile = true
