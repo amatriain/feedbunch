6aa35eed277df2b6b623dff48cb2ae9f739efa57
Alfredo Amatriain <geralt@gmail.com>
2015-03-20 20:47:22 +0100

    Reorganization of tests.

--- spec/unit_tests/workers/reset_demo_user_worker_spec.rb
+++ spec/unit_tests/workers/reset_demo_user_worker_spec.rb
@@ -3,55 +3,54 @@ require 'rails_helper'
 describe ResetDemoUserWorker do
 
   before :each do
-    Feedbunch::Application.config.demo_enabled = true
     @demo_email = Feedbunch::Application.config.demo_email
     @demo_password = Feedbunch::Application.config.demo_password
   end
 
-  context 'create demo user' do
+  context 'demo user disabled' do
 
-    context 'demo user enabled' do
-
-      it 'creates demo user if it does not exist' do
-        expect(User.find_by_email @demo_email).to be nil
-        ResetDemoUserWorker.new.perform
-        expect(User.find_by_email @demo_email).not_to be nil
-      end
-
-      it 'does not alter demo user if it exists' do
-        demo_user = FactoryGirl.create :user, email: @demo_email, password: @demo_password
-
-        expect(User.find_by_email @demo_email).not_to be nil
-        ResetDemoUserWorker.new.perform
-        expect(User.find_by_email @demo_email).to eq demo_user
-      end
+    before :each do
+      Feedbunch::Application.config.demo_enabled = false
     end
 
-    context 'demo user disabled' do
+    it 'does not create demo user' do
+      expect(User.find_by_email @demo_email).to be nil
+      ResetDemoUserWorker.new.perform
+      expect(User.find_by_email @demo_email).to be nil
+    end
 
-      before :each do
-        Feedbunch::Application.config.demo_enabled = false
-      end
+    it 'destroys demo user if it exists' do
+      demo_user = FactoryGirl.create :user, email: @demo_email, password: @demo_password
 
-      it 'does not create demo user' do
-        expect(User.find_by_email @demo_email).to be nil
-        ResetDemoUserWorker.new.perform
-        expect(User.find_by_email @demo_email).to be nil
-      end
-
-      it 'destroys demo user if it exists' do
-        demo_user = FactoryGirl.create :user, email: @demo_email, password: @demo_password
-
-        expect(User.find_by_email @demo_email).not_to be nil
-        ResetDemoUserWorker.new.perform
-        expect(User.find_by_email @demo_email).to be nil
-      end
-
+      expect(User.find_by_email @demo_email).not_to be nil
+      ResetDemoUserWorker.new.perform
+      expect(User.find_by_email @demo_email).to be nil
     end
   end
 
-  context 'reset demo user' do
-    # TODO implement
+  context 'demo user enabled' do
+
+    before :each do
+      Feedbunch::Application.config.demo_enabled = true
+    end
+
+    it 'creates demo user if it does not exist' do
+      expect(User.find_by_email @demo_email).to be nil
+      ResetDemoUserWorker.new.perform
+      expect(User.find_by_email @demo_email).not_to be nil
+    end
+
+    it 'does not alter demo user if it exists' do
+      demo_user = FactoryGirl.create :user, email: @demo_email, password: @demo_password
+
+      expect(User.find_by_email @demo_email).not_to be nil
+      ResetDemoUserWorker.new.perform
+      expect(User.find_by_email @demo_email).to eq demo_user
+    end
+
+    context 'reset demo user' do
+      # TODO implement
+    end
   end
 
 end
