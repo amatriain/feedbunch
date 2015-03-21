ff3d130105eaab50f5c9c7e7734a81645ffbd9fb
Alfredo Amatriain <geralt@gmail.com>
2015-03-21 11:56:38 +0100

    WIP: acceptance tests for the demo user feature.

--- spec/acceptance_tests/suite_1/demo_user_spec.rb
+++ spec/acceptance_tests/suite_1/demo_user_spec.rb
@@ -2,15 +2,33 @@ require 'rails_helper'
 
 describe 'demo user', type: :feature do
 
-  before :each do
+  context 'demo disabled' do
 
+    before :each do
+      Feedbunch::Application.config.demo_enabled = false
+    end
+
+    it 'does not show a link to the demo', js: true do
+      visit root_path
+    end
   end
 
-  it 'cannot change his email'
+  context 'demo enabled' do
 
-  it 'cannot change his password'
+    before :each do
+      Feedbunch::Application.config.demo_enabled = true
+    end
 
-  it 'cannot be locked because of authentication failures'
+    it 'shows a link to the demo', js: true do
+      visit root_path
+    end
+
+    it 'cannot change his email'
+
+    it 'cannot change his password'
+
+    it 'cannot be locked because of authentication failures'
+  end
 
   # TODO: implement acceptance tests
 end
