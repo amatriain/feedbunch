0dc68c4fba19d1682a4b5660076ec1d55b7a0f06
amatriain <geralt@gmail.com>
2015-04-07 15:46:54 +0200

    Added superworker expiration config to sidekiq-superworker config.
    This option is new since 1.2.0

--- config/initializers/sidekiq-superworkers.rb
+++ config/initializers/sidekiq-superworkers.rb
@@ -1,5 +1,9 @@
 # Define superworkers for the sidekiq-superworker gem.
 
+# If subworkers die, superworker is never notified. Setting this option makes the superworker expire
+# after N seconds.
+Sidekiq::Superworker.options[:superjob_expiration] = 2592000 # 1 Month
+
 Superworker.define :ImportSubscriptionsWorker, :opml_import_job_state_id, :urls, :folder_ids do
   batch urls: :url, folder_ids: :folder_id do
     ImportSubscriptionWorker :opml_import_job_state_id, :url, :folder_id
