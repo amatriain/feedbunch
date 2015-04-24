580ae2a8c6cf9195b0136947ee163d0ab9b04a2d
amatriain <geralt@gmail.com>
2015-04-24 15:48:53 +0200

    Minor cosmetic change in code (double to single quotes).

--- lib/signups_manager.rb
+++ lib/signups_manager.rb
@@ -51,7 +51,7 @@ class SignupsManager
     Rails.logger.info "Sending first confirmation reminder to unconfirmed users signed up before #{signups_older_than}"
 
     old_unconfirmed_signups = User.
-        where "confirmed_at is null AND confirmation_sent_at < ? AND first_confirmation_reminder_sent = ? AND invitation_sent_at is null",
+        where 'confirmed_at is null AND confirmation_sent_at < ? AND first_confirmation_reminder_sent = ? AND invitation_sent_at is null',
               signups_older_than, false
 
     if old_unconfirmed_signups.empty?
@@ -78,7 +78,7 @@ class SignupsManager
     Rails.logger.info "Sending second confirmation reminder to unconfirmed users signed up before #{signups_older_than}"
 
     old_unconfirmed_signups = User.
-        where "confirmed_at is null AND confirmation_sent_at < ? AND second_confirmation_reminder_sent = ? AND invitation_sent_at is null",
+        where 'confirmed_at is null AND confirmation_sent_at < ? AND second_confirmation_reminder_sent = ? AND invitation_sent_at is null',
               signups_older_than, false
 
     if old_unconfirmed_signups.empty?
