a02f4eebe97e8dc6693418cc4f00bb9d6b5477a6
amatriain <geralt@gmail.com>
2015-05-20 13:40:59 +0200

    Display localized strings in the kb-shortcuts help popup for special keys (esc, spacebar etc).

--- lib/keycode_to_string_converter.rb
+++ lib/keycode_to_string_converter.rb
@@ -19,11 +19,58 @@ class KeycodeToStringConverter
   # Returns the string representation of the passed code.
 
   def self.convert(code, locale)
-    str = code.chr
-    if str =~ /^[[:alnum:]]$/
-      return str
+    if code.chr =~ /^[[:alnum:]]$/
+      str = code.chr
     else
-      # TODO
+      unless I18n.available_locales.include? locale
+        Rails.logger.info "Attempting to convert keycode #{code} to string with unavailable locale #{locale}. Defaulting to locale 'en' instead"
+        locale = 'en'
+      end
+
+      case code
+        when 8
+          str = I18n.t 'keys.backspace'
+        when 9
+          str = I18n.t 'keys.tab'
+        when 13
+          str = I18n.t 'keys.enter'
+        when 16
+          str = I18n.t 'keys.shift'
+        when 17
+          str = I18n.t 'keys.ctrl'
+        when 18
+          str = I18n.t 'keys.alt'
+        when 20
+          str = I18n.t 'keys.capslock'
+        when 27
+          str = I18n.t 'keys.escape'
+        when 32
+          str = I18n.t 'keys.spacebar'
+        when 33
+          str = I18n.t 'keys.pageup'
+        when 34
+          str = I18n.t 'keys.pagedown'
+        when 35
+          str = I18n.t 'keys.end'
+        when 36
+          str = I18n.t 'keys.home'
+        when 37
+          str = I18n.t 'keys.arrow_left'
+        when 38
+          str = I18n.t 'keys.arrow_up'
+        when 39
+          str = I18n.t 'keys.arrow_right'
+        when 40
+          str = I18n.t 'keys.arrow_down'
+        when 45
+          str = I18n.t 'keys.insert'
+        when 46
+          str = I18n.t 'keys.delete'
+        else
+          str = '???'
+      end
     end
+
+    return str
   end
 end
