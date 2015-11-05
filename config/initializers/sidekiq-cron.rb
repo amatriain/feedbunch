73479051ac2d9c76eea163bcd12e47038c2dafd2
Alfredo Amatriain <geralt@gmail.com>
2015-11-06 00:21:46 +0100

    Wrap sidekiq-cron initialization inside a Sidekiq configure block.
    This should remove errors when deploying with capistrano. Inspired by Sidekiq-cron docs.

--- config/initializers/sidekiq-cron.rb
+++ config/initializers/sidekiq-cron.rb
@@ -2,32 +2,34 @@
 #
 # cron format: (minute hour day-of-month month day-of-week)
 
-Sidekiq::Cron::Job.create name: 'Cleanup old invitations - daily at 2AM',
-                          cron: '0 2 * * *',
-                          klass: 'CleanupInvitationsWorker',
-                          queue: :maintenance
+Sidekiq.configure_server do |config|
+  Sidekiq::Cron::Job.create name: 'Cleanup old invitations - daily at 2AM',
+                            cron: '0 2 * * *',
+                            klass: 'CleanupInvitationsWorker',
+                            queue: :maintenance
 
-Sidekiq::Cron::Job.create name: 'Cleanup old signups - daily at 3AM',
-                          cron: '0 3 * * *',
-                          klass: 'CleanupSignupsWorker',
-                          queue: :maintenance
+  Sidekiq::Cron::Job.create name: 'Cleanup old signups - daily at 3AM',
+                            cron: '0 3 * * *',
+                            klass: 'CleanupSignupsWorker',
+                            queue: :maintenance
 
-Sidekiq::Cron::Job.create name: 'Cleanup old signups - daily at 4AM',
-                          cron: '0 4 * * *',
-                          klass: 'DestroyOldJobStatesWorker',
-                          queue: :maintenance
+  Sidekiq::Cron::Job.create name: 'Cleanup old signups - daily at 4AM',
+                            cron: '0 4 * * *',
+                            klass: 'DestroyOldJobStatesWorker',
+                            queue: :maintenance
 
-Sidekiq::Cron::Job.create name: 'Restore missing scheduled feed updates - daily at 5AM',
-                          cron: '0 5 * * *',
-                          klass: 'FixSchedulesWorker',
-                          queue: :maintenance
+  Sidekiq::Cron::Job.create name: 'Restore missing scheduled feed updates - daily at 5AM',
+                            cron: '0 5 * * *',
+                            klass: 'FixSchedulesWorker',
+                            queue: :maintenance
 
-Sidekiq::Cron::Job.create name: 'Reset the demo user - hourly',
-                          cron: '0 * * * *',
-                          klass: 'ResetDemoUserWorker',
-                          queue: :maintenance
+  Sidekiq::Cron::Job.create name: 'Reset the demo user - hourly',
+                            cron: '0 * * * *',
+                            klass: 'ResetDemoUserWorker',
+                            queue: :maintenance
 
-Sidekiq::Cron::Job.create name: 'Update the statistics of the Rails cache - every 10 minutes',
-                          cron: '*/10 * * * *',
-                          klass: 'UpdateRedisCacheStatsWorker',
-                          queue: :maintenance
+  Sidekiq::Cron::Job.create name: 'Update the statistics of the Rails cache - every 10 minutes',
+                            cron: '*/10 * * * *',
+                            klass: 'UpdateRedisCacheStatsWorker',
+                            queue: :maintenance
+end
