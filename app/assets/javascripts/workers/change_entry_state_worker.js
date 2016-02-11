7c52f9b15d635eba68ff110d7ef1fbade39a0d91
Alfredo Amatriain <geralt@gmail.com>
2016-02-12 00:47:44 +0100

    Changing the state (read/unread) of a single entry is performed in a web worker.
    This means the main thread is not used for this ajax call, which makes the UI a bit more responsive, specially in multicore devices.
    
    This change does not improve performace a lot, because this ajax call is not resource intensive. However it will be the blueprint for moving other processes to web workers (at the very least, all ajax calls, both scheduled and user initiated).
    
    Also the new architecture based on web workers has allowed implementing a new feature: if there is a network failure during the ajax call, instead of immediately showing an error to the user the ajax call is reattempted a number of times, before giving up. This hides intermittent network failures from the user.

--- app/assets/javascripts/workers/change_entry_state_worker.js
+++ app/assets/javascripts/workers/change_entry_state_worker.js
@@ -2,9 +2,54 @@
  * Web Worker to update the state of a single entry in a separate thread.
  */
 
+// Maximum number of times the http POST is attempted
+max_retries = 60
+
+// Interval between HTTP POST retries
+retry_interval_msec = 1000
+
+// Callback for messages from the main thread
 onmessage = function(e){
+    // CSRF token
+    var token = e.data.token
+
+    // ID of the entry
+    var id = e.data.id
+
+    // New state for the entry, "read" or "unread"
+    var state = e.data.state
+
+    // ID of the feed, if the user clicked on the feed link in the entry.
+    // Not used in this worker, but returned to the main thread so it knows how to
+    // act when receiving the message callback.
+    var feed_id = e.data.feed_id
+
+    do_post(token, id, state, feed_id, 0);
+}
+
+// Perform the HTTP POST
+do_post = function(token, id, state, feed_id, retry_count) {
     var req = new XMLHttpRequest();
-
-
-    postMessage(e.data);
+    req.onreadystatechange = function(e) {
+        if (req.readyState == XMLHttpRequest.DONE) {
+            if (req.status == 0) {
+                // Network error, retry up to max_retries times
+                if (retry_count < max_retries) {
+                    setTimeout(do_post, retry_interval_msec, token, id, state, feed_id, retry_count + 1);
+                }
+                else {
+                    // Unrecoverable failure
+                    postMessage({status: req.status, id: id, feed_id: feed_id});
+                }
+            }
+            else {
+                // Success (actual HTTP status may indicate an error response, main thread handles it)
+                postMessage({status: req.status, id: id, feed_id: feed_id});
+            }
+        }
+    };
+    req.open("PUT", "/api/entries/update.json");
+    req.setRequestHeader("X-CSRF-Token", token);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send( JSON.stringify( {entry: {id: id, state: state}} ) );
 }
