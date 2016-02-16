e0eca815cefebb7d3084ce7a1cf87155af7e98ce
amatriain <geralt@gmail.com>
2016-02-16 11:00:43 +0100

    User data are loaded with a web worker.

--- app/assets/javascripts/workers/load_user_data_worker.js
+++ app/assets/javascripts/workers/load_user_data_worker.js
@@ -17,27 +17,28 @@ onmessage = function(e){
 
 // Perform the HTTP GET
 do_get = function(token, retry_count) {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function(e) {
-        if (req.readyState == XMLHttpRequest.DONE) {
-            if (req.status == 0) {
-                // Network error, retry up to max_retries times
-                if (retry_count < max_retries) {
-                    setTimeout(do_get, retry_interval_msec, token, retry_count + 1);
-                }
-                else {
-                    // Unrecoverable failure
-                    postMessage({status: req.status});
-                }
-            }
-            else {
-                // Success (actual HTTP status may indicate an error response, main thread handles it)
-                postMessage({status: req.status});
-            }
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function(e) {
+    if (req.readyState == XMLHttpRequest.DONE) {
+      if (req.status == 0) {
+        // Network error, retry up to max_retries times
+        if (retry_count < max_retries) {
+            setTimeout(do_get, retry_interval_msec, token, retry_count + 1);
         }
-    };
-    req.open("GET", "/api/user_data.json");
-    req.setRequestHeader("X-CSRF-Token", token);
-    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    req.send();
+        else {
+            // Unrecoverable failure
+            postMessage({status: req.status});
+        }
+      }
+      else {
+        // Success (actual HTTP status may indicate an error response, main thread handles it)
+        jsonResponse = JSON.parse(req.responseText);
+        postMessage({status: req.status, response: jsonResponse});
+      }
+    }
+  };
+  req.open("GET", "/api/user_data.json");
+  req.setRequestHeader("X-CSRF-Token", token);
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  req.send();
 }
