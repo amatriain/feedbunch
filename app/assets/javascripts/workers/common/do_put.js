71dde40e9653aebf34797873766e5975c1f95bf4
amatriain <geralt@gmail.com>
2016-02-16 16:30:50 +0100

    Same change as 285b91983ef9fcd8c7e36d7bced46683a19ed270 but for workers that perform PUT requests.

--- app/assets/javascripts/workers/common/do_put.js
+++ app/assets/javascripts/workers/common/do_put.js
@@ -1,5 +1,5 @@
 /**
- * Perform an HTTP GET from a web worker
+ * Perform an HTTP PUT from a web worker
  * Built-in retrying if the network is down
  */
 
@@ -9,33 +9,29 @@ max_retries = 60;
 // Interval between HTTP POST retries
 retry_interval_msec = 1000;
 
-// Perform the HTTP GET
-do_get = function(url, token, retry_count) {
+// Perform the HTTP POST
+do_put = function(url, token, data, retry_count) {
   var req = new XMLHttpRequest();
   req.onreadystatechange = function(e) {
     if (req.readyState == XMLHttpRequest.DONE) {
       if (req.status == 0) {
         // Network error, retry up to max_retries times
         if (retry_count < max_retries) {
-          setTimeout(do_get, retry_interval_msec, url, token, retry_count + 1);
+          setTimeout(do_post, retry_interval_msec, url, token, data, retry_count + 1);
         }
         else {
           // Unrecoverable failure
-          postMessage({status: req.status});
+          postMessage({status: req.status, data: data});
         }
       }
       else {
         // Success (actual HTTP status may indicate an error response, main thread handles it)
-        data = {status: req.status};
-        if (req.responseText){
-          data['response'] = JSON.parse(req.responseText);
-        }
-        postMessage(data);
+        postMessage({status: req.status, data: data});
       }
     }
   };
-  req.open("GET", url);
+  req.open("PUT", url);
   req.setRequestHeader("X-CSRF-Token", token);
   req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  req.send();
+  req.send( JSON.stringify(data) );
 }
