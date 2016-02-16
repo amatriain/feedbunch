6e7e69e12be53fb19ef59ae394f3209c4150227b
Alfredo Amatriain <geralt@gmail.com>
2016-02-17 00:13:26 +0100

    Deleting a refresh feed job state is done in a web worker.

---
+++ app/assets/javascripts/workers/common/do_delete.js
@@ -0,0 +1,37 @@
+/**
+ * Perform an HTTP DELETE from a web worker
+ * Built-in retrying if the network is down
+ */
+
+// Maximum number of times the http DELETE is attempted
+max_retries = 60;
+
+// Interval between HTTP DELETE retries
+retry_interval_msec = 1000;
+
+// Perform the HTTP DELETE
+do_delete = function(url, token, retry_count) {
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function(e) {
+    if (req.readyState == XMLHttpRequest.DONE) {
+      if (req.status == 0) {
+        // Network error, retry up to max_retries times
+        if (retry_count < max_retries) {
+          setTimeout(do_delete, retry_interval_msec, url, token, retry_count + 1);
+        }
+        else {
+          // Unrecoverable failure
+          postMessage({status: req.status});
+        }
+      }
+      else {
+        // Success (actual HTTP status may indicate an error response, main thread handles it)
+        postMessage({status: req.status});
+      }
+    }
+  };
+  req.open("DELETE", url);
+  req.setRequestHeader("X-CSRF-Token", token);
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  req.send();
+}
