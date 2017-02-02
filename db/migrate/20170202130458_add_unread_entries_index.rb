23514fa366836c621835ead5c3e2e85437a936b7
amatriain <geralt@gmail.com>
2017-02-02 14:20:20 +0100

    Added partial index to speed up retrieving unread entries in the same order they will be presented to the user.

---
+++ db/migrate/20170202130458_add_unread_entries_index.rb
@@ -0,0 +1,8 @@
+class AddUnreadEntriesIndex < ActiveRecord::Migration[5.0]
+  def change
+    add_index :entry_states, [:published, :entry_created_at, :entry_id],
+              order: {published: :desc, entry_created_at: :desc, entry_id: :desc},
+              where: "read = 'false'",
+              name: 'index_entry_states_unread_on_order_fields'
+  end
+end
