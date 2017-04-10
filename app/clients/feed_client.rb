d435bcb33f44f853a61c687931d9d8e81000b317
Alfredo Amatriain <geralt@gmail.com>
2017-04-11 01:02:31 +0200

    Use the file backend for the rack-cache entity store.
    The noop backend is causing problems. There is a feed
    (http://oglaf.com/feeds/rss/) that for some reason is being treated as
    "stale, invalid" (i.e. the http caching decides it must be
    re-downloaded) but instead of returning the actual feed body to the
    invoker, it receives a blank string instead. I cannot reproduce it in
    development, in other enviroments different from production I'm getting
    the expected "stale, valid" trace and therefore the body is irrelevant,
    but in production there is something different that caused the trace to
    have an "invalid" at the end despite returing an empty body.
    
    I'll have to implement cron scripts to cleanup the hard drive and ensure
    not too much space is taken up by the entity store, which never cleans
    up itself. Maybe deleting entities older than a month.

--- app/clients/feed_client.rb
+++ app/clients/feed_client.rb
@@ -93,7 +93,7 @@ class FeedClient
       RestClient.enable Rack::Cache,
                         verbose: false,
                         metastore: "file:#{Rails.root.join('rack_cache', 'metastore').to_s}",
-                        entitystore: 'noop://'
+                        entitystore: "file:#{Rails.root.join('rack_cache', 'entitystore').to_s}"
     else
       Rails.logger.info "Fetching feed #{feed.id} - fetch_URL #{feed.fetch_url} without HTTP caching"
       RestClient.disable Rack::Cache
