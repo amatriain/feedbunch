b55d5dc4ee2df32c829b1dd0c0d4cdd752443494
Alfredo Amatriain <geralt@gmail.com>
2017-12-17 21:45:27 +0100

    Updated to ruby-2.4.3
    This fixes a vulnerability in Net::FTP
    See https://www.ruby-lang.org/en/news/2017/12/14/net-ftp-command-injection-cve-2017-17405/

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :application, 'feedbunch'
 #############################################################
 
 set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.4.2'
+set :rvm_ruby_version, 'ruby-2.4.3'
 set :rvm_user_path, '~/.rvm'
 
 #############################################################
