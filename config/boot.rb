da1f8162bd0893eff7977597ca6b94617b01d891
Alfredo Amatriain <geralt@gmail.com>
2019-03-14 01:42:00 +0100

    Revert "Activate latest bundler version on boot"
    This reverts commit 2c945f96114367b99ef9999473de07b8025eafb0.

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-Gem::Specification.find_by_name('bundler').activate # activate latest bundler version
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Faster startup
