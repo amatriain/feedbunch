2c945f96114367b99ef9999473de07b8025eafb0
Alfredo Amatriain <geralt@gmail.com>
2019-03-14 01:29:59 +0100

    Activate latest bundler version on boot
    I'm getting a bug in CI environment about wrong bundler version being
    activated. Hopefully this fixes it.

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+Gem::Specification.find_by_name('bundler').activate # activate latest bundler version
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Faster startup
