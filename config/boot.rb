69431d5c6c13b371d0e6fbbd98cbfbf20ea405e7
Alfredo Amatriain <geralt@gmail.com>
2019-03-14 01:33:40 +0100

    Bundler version bug still not fixed

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-Gem::Specification.find_by_name('bundler').activate # activate latest bundler version
+Gem::Specification.find_by_name('bundler', '~> 2.0.1').activate # activate latest bundler version
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Faster startup
