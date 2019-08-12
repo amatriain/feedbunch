736622f66e792e5934354a638f80709813baf561
amatriain <geralt@gmail.com>
2019-08-12 16:44:44 +0200

    Added unique_hash attribute to deleted entry factory

--- spec/factories/deleted_entries.rb
+++ spec/factories/deleted_entries.rb
@@ -2,9 +2,11 @@
 
 FactoryBot.define do
   sequence(:deleted_entry_guid_sequence) {|n| "some_entry_guid_#{n}"}
+  sequence(:unique_hash_sequene){|n| "abcd123_#{n}"}
 
   factory :deleted_entry do
     guid {generate :deleted_entry_guid_sequence}
+    unique_hash {generate :unique_hash_sequene}
     feed
   end
 end
