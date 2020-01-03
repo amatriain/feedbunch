2b9ee884ba70e30090d81d57b5e26d7f238f6c7f
amatriain <geralt@gmail.com>
2020-01-03 14:19:03 +0100

    New config defaults for Rails 6

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,10 @@ module Feedbunch
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults '6.0'
+
+    # Force SSL for all endpoints
+    config.force_ssl = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
