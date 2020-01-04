75235ba752588008ef6683534fee5acb89140614
Alfredo Amatriain <geralt@gmail.com>
2020-01-04 11:49:22 +0100

    Disabled force_ssl in application.rb
    It breaks development, testing and CI.

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,6 @@ module Feedbunch
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults '6.0'
 
-    # Force SSL for all endpoints
-    config.force_ssl = true
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
