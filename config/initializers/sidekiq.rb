ea4d3862db3e4fbd7d758bcefd2c7176e1c7f0b9
Alfredo Amatriain <geralt@gmail.com>
2021-01-14 23:33:02 +0100

    Use a single sidekiq server initialization block
    FeedBunch was making two calls to Sidekiq.configure_server : one to
    initialize the redis connection pool and a second one to create default
    scheduled jobs. There is a possibility that this was causing a bug in
    which Sidekiq always tried to connect to Redis in localhost:6379,
    regardless of what was in the configuration. Hopefully using a single
    configure_server call avoids this.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,6 +12,33 @@ redis_conn = proc {
 Sidekiq.configure_server do |config|
   # Server needs (concurrency + 2) redis connections
   config.redis = ConnectionPool.new size: 12, &redis_conn
+
+  # Declare sidekiq-cron jobs (sidekiq workers that must be run periodically)
+  # cron format: (minute hour day-of-month month day-of-week)
+  Sidekiq::Cron::Job.create name: 'Cleanup old signups - daily at 3AM',
+                            cron: '0 3 * * *',
+                            klass: 'CleanupSignupsWorker',
+                            queue: :maintenance
+
+  Sidekiq::Cron::Job.create name: 'Destroy old job states - daily at 4AM',
+                            cron: '0 4 * * *',
+                            klass: 'DestroyOldJobStatesWorker',
+                            queue: :maintenance
+
+  Sidekiq::Cron::Job.create name: 'Restore missing scheduled feed updates - daily at 6AM',
+                            cron: '0 6 * * *',
+                            klass: 'FixSchedulesWorker',
+                            queue: :maintenance
+
+  Sidekiq::Cron::Job.create name: 'Reset the demo user - hourly',
+                            cron: '0 * * * *',
+                            klass: 'ResetDemoUserWorker',
+                            queue: :maintenance
+
+  Sidekiq::Cron::Job.create name: 'Update the statistics of the Rails cache - every 10 minutes',
+                            cron: '*/10 * * * *',
+                            klass: 'UpdateRedisCacheStatsWorker',
+                            queue: :maintenance
 end
 
 Sidekiq.configure_client do |config|
