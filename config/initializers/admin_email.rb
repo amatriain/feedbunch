f3b0950da7a3ad4d076b2aeb84d70a1f76ef301d
amatriain <geralt@gmail.com>
2021-01-22 13:02:27 +0100

    Make the admin email parametrizable.
    It can be set with the ADMIN_EMAIL environment variable, it takes the
    value 'admin@feedbunch.com' by default.
    
    This email is used when sending certain emails from the app, like OPML
    import/export failure notifications and emails related to Devise
    authentication workflows.

--- config/initializers/admin_email.rb
+++ config/initializers/admin_email.rb
@@ -1,2 +1,4 @@
-# Admin email
-Rails.application.config.admin_email = 'admin@feedbunch.com'
+# Admin email can be set with the "ADMIN_EMAIL" environment variable.
+# It takes the value admin@feedbunch.com by default.
+admin_email = ENV.fetch("ADMIN_EMAIL") { 'admin@feedbunch.com' }
+Rails.application.config.admin_email = admin_email
