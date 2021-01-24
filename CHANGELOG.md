a62d35926f88bd10401c05a703a7a106ed4684d8
Alfredo Amatriain <geralt@gmail.com>
2021-01-24 12:57:33 +0100

    Fixed: rails env is not unconditionally set to production anymore

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Changed 
 Nothing yet
+### Fixed
+- Rails env is not unconditionally set to production by the devise initializer anymore.
 
 ## [1.3.18] - 2021-01-22
 ### Changed 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
     # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  if Rails.env = 'production'
+  if Rails.env == 'production'
     config.mailer_sender = "\"FeedBunch\" <#{Feedbunch::Application.config.admin_email}>"
   else
     config.mailer_sender = 'root@localhost'
