b04495947a4012ddf6ce4c9c4cb87e9e43ebee13
Alfredo Amatriain <geralt@gmail.com>
2021-01-24 13:39:41 +0100

    Remove foreign keys created by a recent migration
    It seems these foreign keys are created by (relatively) recent versions
    of Rails. Older versions did not create them.
    
    They are probably useful and a good idea in the long run, but right now
    they break tests becase FeedBunch has been built assuming those foreign
    keys (and particularly the "on delete cascade" constraints) are not
    there.

--- db/schema.rb
+++ db/schema.rb
@@ -210,20 +210,4 @@ ActiveRecord::Schema.define(version: 2021_01_22_225804) do
     t.index ["second_confirmation_reminder_sent"], name: "index_users_on_second_invitation_reminder_fields"
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
-
-  add_foreign_key "deleted_entries", "feeds", on_delete: :cascade
-  add_foreign_key "entries", "feeds", on_delete: :cascade
-  add_foreign_key "entry_states", "entries", on_delete: :cascade
-  add_foreign_key "entry_states", "users", on_delete: :cascade
-  add_foreign_key "feed_subscriptions", "feeds", on_delete: :cascade
-  add_foreign_key "feed_subscriptions", "users", on_delete: :cascade
-  add_foreign_key "feeds_folders", "feeds", on_delete: :cascade
-  add_foreign_key "feeds_folders", "folders", on_delete: :cascade
-  add_foreign_key "folders", "users", on_delete: :cascade
-  add_foreign_key "opml_export_job_states", "users", on_delete: :cascade
-  add_foreign_key "opml_import_failures", "opml_import_job_states", on_delete: :cascade
-  add_foreign_key "opml_import_job_states", "users", on_delete: :cascade
-  add_foreign_key "refresh_feed_job_states", "feeds", on_delete: :cascade
-  add_foreign_key "refresh_feed_job_states", "users", on_delete: :cascade
-  add_foreign_key "subscribe_job_states", "users", on_delete: :cascade
 end
