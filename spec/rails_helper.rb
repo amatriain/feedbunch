6194f3c4c7e9043b3525da07f520f3b1f2c92e28
amatriain <geralt@gmail.com>
2021-01-28 12:43:54 +0100

    Removed coveralls gem code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'coveralls'
-Coveralls.wear!('rails')
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
